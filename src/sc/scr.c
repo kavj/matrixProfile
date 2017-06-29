ded43b36bd867dc03a40d2281f3bab3e723b7dab
kavj <kjkamgar@gmail.com>
2017-06-29 01:24:03 -0700

    more adjustments

--- src/sc/scr.c
+++ src/sc/scr.c
@@ -76,9 +76,9 @@ static double incQ(double Q, double M, double x, int k){
 }
 
 
-/* This is based on Higham's approach to the rolling mean and variance, which I think is based on Welford's method for online variance
- * It's easy enough to simultaneously accumulate the means on a pass through the data, so I went ahead and did that. 
- */
+
+/* This uses a variation on Welford's method for sample variance to compute the mean and standard deviation. 
+ * It resets the summation once the term under consideration does not share any terms with the last exact summation. */
 int winmeansig(const double* T, double* mu, double* sigma, int n, int m){
     double M = T[0];
     double Q = 0;
@@ -92,17 +92,59 @@ int winmeansig(const double* T, double* mu, double* sigma, int n, int m){
         int w = (i < n-2*m+1)? i+m : n;
         double Q_prev = Q;
         double M_prev = M;
+        Q = 0;
+        M = T[i+1];
         for(int j = i; j < w; j++){
-
+            if(j != i){
+                Q = incQ(Q,M,T[j+m],j-i+1);
+                M = incM(M,T[j+m],j-i+1);
+            }
+            M_prev = decM(M_prev,T[j],m);
+            Q_prev = decQ(Q_prev,M_prev,T[j],m);
+            Q_prev = incQ(Q_prev,M_prev,T[j+m],m);
+            M_prev = incM(M_prev,T[j+subLen],m);
+            mu[j+1] = M_prev;
+            sigma[j+1] = sqrt(Q_prev/m);
         }
 
     }
     return 0;
 }
 
-/*  We don't use an explicit loop kernel. Since relative alignment is completely unknown due to T being passed in externally and compared against an arbitrary lag and subsequence length,
- *  it's too difficult to guarantee a lack of protection faults. Instead I felt it might be better to limit the amount of memory accessed. In general the compiler will turn these temporary variables
- *  into register variables and this will run over a subset of T on any given call. The general assumption is that these calls will be blocked against L2, which will accommodate most subsequence lengths.
- *  This doesn't attempt to do any kind of vectorization. I even removed it from the min operation, because this eliminates a data dependency. It won't wait on mpI if the branch is not taken. */
+static double distInc(double a, double b, double c){
+    return a + b*c;
+}
 
+static double distDec(double a, double b, double c){
+    return a - b*c;
+}
 
+static double znorm(double p, double mu_s, double mu_l, double sigma_l, double sigma_l){
+    return (p - mu_s*mu_l)/(sigma_s*sigma_l);
+}
+
+void mpSelf(const double* T, const double* mu, const double* sigma, double* mp, int* mpI, const int n, const int m){
+    double x = 0;
+    for(int j = 0; j < m; i++){
+        x += T[i]*T[i+lag];
+    }
+    double z = znorm(x,mu[0],mu[lag],sigma[0],sigma[lag]);
+    for(int i = 0; i < n-m-lag; i+=m){
+        double y = x;
+        x = 0;
+        w = (i < n-2*m-lag) ? i+m : n;
+        for(int j = i; j < w; j++)
+            x = distInc(x,T[j+m],T[lag+j+m]);
+            y = distDec(y,T[j],T[lag+j]);
+            z = znorm(x,mu[j+1],mu[lag+j+1],sigma[j+1],sigma[lag+j+1]);
+            if(z < mp[j+1]){
+                mp[j+1] = z;
+                mpI[j+1] = lag+j+1;
+            }
+            if(z < mp[lag+j+1]){
+                mp[lag+j+1] = z;
+                mpI[lag+j+1] = j+1;
+            }
+        }
+    }
+}
