c0974a47ff8763380c84d13ad956be2e430aa6ae
kavj <kjkamgar@gmail.com>
2017-12-31 10:38:40 -0800

    making corrections

--- src/utils/sumstats.cpp
+++ src/utils/sumstats.cpp
@@ -3,8 +3,7 @@
 
 using namespace vmth;
 typedef __m256d vtf;
-#define vwidth 4
-
+#define wid 4
 
 
 //template <typename dtype>
@@ -32,35 +31,35 @@ vtf vcss_1x1(double* a, vtf mu, int base, int w){
 //static inline void ssinit_8x1(dtype *a, dtype *mu, dtype *s, int base, int w){
 static void vcss_8x1(double* a, double*mu, double* s, int base, int w){
    vtf ma = aload(mu,base);
-   vtf mb = aload(mu,base+vwidth);
-   vtf mc = aload(mu,base+2*vwidth);
-   vtf md = aload(mu,base+3*vwidth);
-   vtf me = aload(mu,base+4*vwidth);
-   vtf mf = aload(mu,base+5*vwidth);
-   vtf mg = aload(mu,base+6*vwidth);
-   vtf mh = aload(mu,base+6*vwidth);
+   vtf mb = aload(mu,base+wid);
+   vtf mc = aload(mu,base+2*wid);
+   vtf md = aload(mu,base+3*wid);
+   vtf me = aload(mu,base+4*wid);
+   vtf mf = aload(mu,base+5*wid);
+   vtf mg = aload(mu,base+6*wid);
+   vtf mh = aload(mu,base+6*wid);
    
 
-   vtf sa = vcss_1x1(a,ma,base,28);
-   vtf sb = vcss_1x1(a,mb,base+4,24);
-   vtf sc = vcss_1x1(a,mc,base+8,20);
-   vtf sd = vcss_1x1(a,md,base+12,16);
-   vtf se = vcss_1x1(a,me,base+16,12);
-   vtf sf = vcss_1x1(a,mf,base+20,8);
-   vtf sg = vcss_1x1(a,mg,base+24,4);
+   vtf sa = vcss_1x1(a,ma,base,7*wid);
+   vtf sb = vcss_1x1(a,mb,base+wid,6*wid);
+   vtf sc = vcss_1x1(a,mc,base+2*wid,5*wid);
+   vtf sd = vcss_1x1(a,md,base+3*wid,4*wid);
+   vtf se = vcss_1x1(a,me,base+4*wid,3*wid);
+   vtf sf = vcss_1x1(a,mf,base+5*wid,2*wid);
+   vtf sg = vcss_1x1(a,mg,base+6*wid,wid);
    vtf sh = setzero();
 
-   for(int i = base; i < base+w+28; i++){
+   for(int i = base+7*wid; i < base+w-7*wid; i++){
       vtf ba = uload(a,i);
 
-      vtf ca = sub(ba,uload(mu,i));
-      vtf cb = sub(ba,uload(mu,i+vwidth));
-      vtf cc = sub(ba,uload(mu,i+2*vwidth));
-      vtf cd = sub(ba,uload(mu,i+3*vwidth));
-      vtf ce = sub(ba,uload(mu,i+4*vwidth));
-      vtf cf = sub(ba,uload(mu,i+5*vwidth));
-      vtf cg = sub(ba,uload(mu,i+6*vwidth));
-      vtf ch = sub(ba,uload(mu,i+7*vwidth));
+      vtf ca = sub(ba,ma);
+      vtf cb = sub(ba,mb);
+      vtf cc = sub(ba,mc);
+      vtf cd = sub(ba,md);
+      vtf ce = sub(ba,me);
+      vtf cf = sub(ba,mf);
+      vtf cg = sub(ba,mg);
+      vtf ch = sub(ba,mh);
 
       sa = fma(ca,ca,sa);
       sb = fma(cb,cb,sb);
@@ -73,38 +72,38 @@ static void vcss_8x1(double* a, double*mu, double* s, int base, int w){
    }
    
    astore(sa,s,base);
-   astore(sb,s,base+vwidth);
-   astore(sc,s,base+2*vwidth);
-   astore(sd,s,base+3*vwidth);
-   astore(se,s,base+4*vwidth);
-   astore(sf,s,base+5*vwidth);
-   astore(sg,s,base+6*vwidth);
-   astore(sh,s,base+7*vwidth);
+   astore(sb,s,base+wid);
+   astore(sc,s,base+2*wid);
+   astore(sd,s,base+3*wid);
+   astore(se,s,base+4*wid);
+   astore(sf,s,base+5*wid);
+   astore(sg,s,base+6*wid);
+   astore(sh,s,base+7*wid);
 }
 
 //template <typename dtype>
 //static inline void ssinit_8x1(dtype *a, dtype *mu, dtype *s, int base, int w){
 static void vcsp_8x1(double* a, double*mu, double* s, int base, int offset, int w){
    vtf sa = uload(a,base);
-   vtf sb = uload(a,base+4);
-   vtf sc = uload(a,base+8);
-   vtf sd = uload(a,base+12);
-   vtf se = uload(a,base+16);
-   vtf sf = uload(a,base+20);
-   vtf sg = uload(a,base+24);
-   vtf sh = uload(a,base+28);
+   vtf sb = uload(a,base+wid);
+   vtf sc = uload(a,base+2*wid);
+   vtf sd = uload(a,base+3*wid);
+   vtf se = uload(a,base+4*wid);
+   vtf sf = uload(a,base+5*wid);
+   vtf sg = uload(a,base+6*wid);
+   vtf sh = uload(a,base+7*wid);
 
    for(int i = base; i < base+w; i++){
       vtf ba = bcast(a,i);
 
       vtf ca = sub(ba,uload(mu,i));
-      vtf cb = sub(ba,uload(mu,i+vwidth));
-      vtf cc = sub(ba,uload(mu,i+2*vwidth));
-      vtf cd = sub(ba,uload(mu,i+3*vwidth));
-      vtf ce = sub(ba,uload(mu,i+4*vwidth));
-      vtf cf = sub(ba,uload(mu,i+5*vwidth));
-      vtf cg = sub(ba,uload(mu,i+6*vwidth));
-      vtf ch = sub(ba,uload(mu,i+7*vwidth));
+      vtf cb = sub(ba,uload(mu,i+wid));
+      vtf cc = sub(ba,uload(mu,i+2*wid));
+      vtf cd = sub(ba,uload(mu,i+3*wid));
+      vtf ce = sub(ba,uload(mu,i+4*wid));
+      vtf cf = sub(ba,uload(mu,i+5*wid));
+      vtf cg = sub(ba,uload(mu,i+6*wid));
+      vtf ch = sub(ba,uload(mu,i+7*wid));
 
       sa = fma(ca,ca,sa);
       sb = fma(cb,cb,sb);
@@ -117,32 +116,30 @@ static void vcsp_8x1(double* a, double*mu, double* s, int base, int offset, int 
    }
    
    astore(sa,s,base);
-   astore(sb,s,base+vwidth);
-   astore(sc,s,base+2*vwidth);
-   astore(sd,s,base+3*vwidth);
-   astore(se,s,base+4*vwidth);
-   astore(sf,s,base+5*vwidth);
-   astore(sg,s,base+6*vwidth);
-   astore(sh,s,base+7*vwidth);
+   astore(sb,s,base+wid);
+   astore(sc,s,base+2*wid);
+   astore(sd,s,base+3*wid);
+   astore(se,s,base+4*wid);
+   astore(sf,s,base+5*wid);
+   astore(sg,s,base+6*wid);
+   astore(sh,s,base+7*wid);
 }
-
-
 
 
 //template <typename dtype>
 //static inline void muinit_8x1(dtype *a, dtype *mu, int base, int w){
 static inline void winmean_8x1(double* a, double* mu, int base, int w){
-   vtf sa = vsum_1x1(a,base,28);
-   vtf sb = vsum_1x1(a,base+4,24);
-   vtf sc = vsum_1x1(a,base+8,20);
-   vtf sd = vsum_1x1(a,base+12,16);
-   vtf se = vsum_1x1(a,base+16,12);
-   vtf sf = vsum_1x1(a,base+20,8);
-   vtf sg = vsum_1x1(a,base+24,4);
+   vtf sa = vsum_1x1(a,base,7*wid);
+   vtf sb = vsum_1x1(a,base+wid,6*wid);
+   vtf sc = vsum_1x1(a,base+2*wid,5*wid);
+   vtf sd = vsum_1x1(a,base+3*wid,4*wid);
+   vtf se = vsum_1x1(a,base+4*wid,3*wid);
+   vtf sf = vsum_1x1(a,base+5*wid,2*wid);
+   vtf sg = vsum_1x1(a,base+6*wid,wid);
    vtf sh = setzero();
 
-   for(int i = base+28; i < base+w; i++){
-      vtf ba = bcast(a,i);
+   for(int i = base+7*wid; i < base+w-7*wid; i++){
+      vtf ba = uload(a,i);
 
       sa = add(sa,ba);
       sb = add(sb,ba);
@@ -155,22 +152,22 @@ static inline void winmean_8x1(double* a, double* mu, int base, int w){
    }
    vtf v = bcast((double)w); 
 
-   sb = add(sb,vsum_1x1(a,base,4));
-   sc = add(sc,vsum_1x1(a,base,8));
-   sd = add(sd,vsum_1x1(a,base,12));
-   se = add(se,vsum_1x1(a,base,16));
-   sf = add(sf,vsum_1x1(a,base,20));
-   sg = add(sg,vsum_1x1(a,base,24)); 
-   sh = add(sh,vsum_1x1(a,base,28));
+   sb = add(sb,vsum_1x1(a,base,wid));
+   sc = add(sc,vsum_1x1(a,base,2*wid));
+   sd = add(sd,vsum_1x1(a,base,3*wid));
+   se = add(se,vsum_1x1(a,base,4*wid));
+   sf = add(sf,vsum_1x1(a,base,5*wid));
+   sg = add(sg,vsum_1x1(a,base,6*wid)); 
+   sh = add(sh,vsum_1x1(a,base,7*wid));
 
    astore(div(sa,v),mu,base);
-   astore(div(sb,v),mu,base+4);
-   astore(div(sc,v),mu,base+8);
-   astore(div(sd,v),mu,base+12);
-   astore(div(se,v),mu,base+16);
-   astore(div(sf,v),mu,base+20);
-   astore(div(sg,v),mu,base+24);
-   astore(div(sh,v),mu,base+28);
+   astore(div(sb,v),mu,base+wid);
+   astore(div(sc,v),mu,base+2*wid);
+   astore(div(sd,v),mu,base+3*wid);
+   astore(div(se,v),mu,base+4*wid);
+   astore(div(sf,v),mu,base+5*wid);
+   astore(div(sg,v),mu,base+6*wid);
+   astore(div(sh,v),mu,base+7*wid);
 
 }
 
