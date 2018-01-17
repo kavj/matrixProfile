0ebca644d37092bbe93782fd91ae05a4bff81299
kavj <kjkamgar@gmail.com>
2018-01-16 23:01:27 -0800

    rearrangin

---
+++ src/kernel/blockarith.hpp
@@ -0,0 +1,324 @@
+#include<cstdio>
+#include "../arch/avx2arith_2.hpp"
+#define simdWid 4
+#define innerWid 64 
+using namespace vmth;
+#define vwidth 4
+typedef  __m256d vtf;
+typedef  __m256i vti;
+
+
+static inline void fma_4x4(vtf &c1, vtf& c2, vtf &c3, vtf &c4, double* dg, double *dh, int i, int j){
+   c1 = fma(bcast(dg,i),aload(dh,j),c1);
+   c2 = fma(bcast(dg,i),aload(dh,j+4),c2);
+   c3 = fma(bcast(dg,i),aload(dh,j+8),c3);
+   c4 = fma(bcast(dg,i),aload(dh,j+12),c4);
+}
+
+
+
+/*static inline void fma_4x4(vtf &c1, vtf& c2, vtf &c3, vtf &c4, vtf &x1, vtf &x2, vtf &x3, vtf &x4, double *d, int i){
+   c1 = fma(x1,aload(d,i),c1);
+   c2 = fma(x2,aload(d,i+4),c2);
+   c3 = fma(x3,aload(d,i+8),c3);
+   c4 = fma(x4,aload(d,i+12),c4);
+}*/
+
+
+
+template<typename dtype, typename vt>
+static inline void fma_8x1(vt &c1, vt& c2, vt &c3, vt &c4, vt &c5, vt &c6, vt &c7, vt &c8, 
+                           dtype *dg, dtype *dh, int i, int j){
+
+   fma_4x4(c1,c2,c3,c4,dg,dh,i,j);
+   fma_4x4(c5,c6,c7,c8,dg,dh,i+16,j+16);
+
+}
+
+
+/*
+template<typename dtype, typename vt>
+static inline void fma_8x1(vt &c1, vt& c2, vt &c3, vt &c4, vt &c5, vt &c6, vt &c7, vt &c8, 
+                           vt &x1, vt &x2, vt &x3, vt &x4, vt &x5, vt &x6, vt &x7, vt &x8, dtype *d, int i){
+
+   fma_4x4(c1,c2,c3,c4,x1,x2,x3,x4,d,i);
+   fma_4x4(c5,c6,c7,c8,x5,x6,x7,x8,d,i+16);
+
+}*/
+
+
+static inline void seti_4x4(vti &c1, vti& c2, vti &c3, vti &c4, 
+                            vtf &x1, vtf &x2, vtf &x3, vtf &x4, int t){
+   
+   vti u = bcast(4);
+   vti v = set(t,t+1,t+2,t+3);
+   c1 = blend(u,c1,x1);
+   u += v;
+   c2 = blend(u,c2,x2);
+   u += v;
+   c3 = blend(u,c3,x3);
+   u += v;
+   c4 = blend(u,c4,x4);
+}
+
+
+
+static inline void seti_8x8(vti &c1, vti& c2, vti &c3, vti &c4, vti &c5, vti &c6, vti &c7, vti &c8, 
+                            vtf &x1, vtf &x2, vtf &x3, vtf &x4, vtf &x5, vtf &x6, vtf &x7, vtf &x8, int t){
+   seti_4x4(c1,c2,c3,c4,x1,x2,x3,x4,t);
+   seti_4x4(c5,c6,c7,c8,x5,x6,x7,x8,t+16);
+}
+
+static inline void cmp_4x4(vtf &c1, vtf &c2, vtf &c3, vtf &c4,
+                           vtf &x1, vtf &x2, vtf &x3, vtf &x4){
+   c1 = cmpgtr(c1,x1);
+   c2 = cmpgtr(c2,x2);
+   c3 = cmpgtr(c3,x3);
+   c4 = cmpgtr(c4,x4);
+}
+
+
+static inline void cmp_8x8(vtf &c1, vtf& c2, vtf &c3, vtf &c4, vtf &c5, vtf &c6, vtf &c7, vtf &c8, 
+                           vtf &x1, vtf &x2, vtf &x3, vtf &x4, vtf &x5, vtf &x6, vtf &x7, vtf &x8){
+   cmp_4x4(c1,c2,c3,c4,x1,x2,x3,x4);
+   cmp_4x4(c5,c6,c7,c8,x5,x6,x7,x8);
+}
+
+
+static inline void max_4x4(vtf &c1, vtf& c2, vtf &c3, vtf &c4, 
+                           vtf &x1, vtf &x2, vtf &x3, vtf &x4){
+   c1 = max(c1,x1);
+   c2 = max(c2,x2);
+   c3 = max(c3,x3);
+   c4 = max(c4,x4);
+}
+
+
+static inline void max_8x8(vtf &c1, vtf& c2, vtf &c3, vtf &c4, vtf &c5, vtf &c6, vtf &c7, vtf &c8, 
+                           vtf &x1, vtf &x2, vtf &x3, vtf &x4, vtf &x5, vtf &x6, vtf &x7, vtf &x8){
+   max_4x4(c1,c2,c3,c4,x1,x2,x3,x4);
+   max_4x4(c5,c6,c7,c8,x5,x6,x7,x8);
+}
+
+
+static inline void prod_8x1(vtf &c1, vtf& c2, vtf &c3, vtf &c4, vtf &c5, vtf &c6, vtf &c7, vtf &c8, 
+                            vtf &x1, vtf &x2, vtf &x3, vtf &x4, vtf &x5, vtf &x6, vtf &x7, vtf &x8){
+   x1 = c1*x1;
+   x2 = c2*x2;
+   x3 = c3*x3;
+   x4 = c4*x4;
+   x5 = c5*x5;
+   x6 = c6*x6;
+   x7 = c7*x7;
+   x8 = c8*x8;
+}
+
+
+static inline void prod_8x1(vtf &d1, vtf &d2, vtf &d3, vtf &d4, vtf &d5, vtf &d6, vtf &d7, vtf &d8, 
+                            vtf &c1, vtf& c2, vtf &c3, vtf &c4, vtf &c5, vtf &c6, vtf &c7, vtf &c8, 
+                            vtf &x1, vtf &x2, vtf &x3, vtf &x4, vtf &x5, vtf &x6, vtf &x7, vtf &x8){
+   d1 = c1*x1;
+   d2 = c2*x2;
+   d3 = c3*x3;
+   d4 = c4*x4;
+   d5 = c5*x5;
+   d6 = c6*x6;
+   d7 = c7*x7;
+   d8 = c8*x8;
+}
+
+static inline void prod_8x1(vtf &d1, vtf &d2, vtf &d3, vtf &d4, vtf &d5, vtf &d6, vtf &d7, vtf &d8, 
+                            vtf &c1, vtf& c2, vtf &c3, vtf &c4, vtf &c5, vtf &c6, vtf &c7, vtf &c8, double *d, int i){
+   d1 = c1 * aload(d,i);
+   d2 = c2 * aload(d,i+4);
+   d3 = c3 * aload(d,i+8);
+   d4 = c4 * aload(d,i+12);
+   d5 = c5 * aload(d,i+16);
+   d6 = c6 * aload(d,i+20);
+   d7 = c7 * aload(d,i+24);
+   d8 = c8 * aload(d,i+28);
+}
+
+static inline void prod_4x4(vtf &c1, vtf& c2, vtf &c3, vtf &c4, vtf &x1, vtf &x2, vtf &x3, vtf &x4){
+   x1 *= c1;
+   x2 *= c2;
+   x3 *= c3;
+   x4 *= c4;
+}
+
+static inline void prod_4x4(vtf &c1, vtf& c2, vtf &c3, vtf &c4, double *d, int i){
+  c1 = c1 * aload(d,i);
+  c2 = c2 * aload(d,i+4);
+  c3 = c3 * aload(d,i+8);
+  c4 = c4 * aload(d,i+12);
+}
+
+
+static inline void prod_8x1(vtf &c1, vtf& c2, vtf &c3, vtf &c4, vtf &c5, vtf &c6, vtf &c7, vtf &c8, double *d, int i){
+   prod_4x4(c1,c2,c3,c4,d,i);
+   prod_4x4(c5,c6,c8,c8,d,i+16);
+}
+
+template<typename dtype, typename vt>
+static inline void load_4x4(vt &c1, vt &c2, vt &c3, vt &c4, dtype *d, int i){
+   c1 = aload(d,i);
+   c2 = aload(d,i+4);
+   c3 = aload(d,i+8);
+   c4 = aload(d,i+12);
+}
+
+template<typename dtype, typename vt>
+static inline void load_8x1(vt &c1, vt &c2, vt &c3, vt &c4, vt &c5, vt &c6, vt &c7, vt &c8, dtype *d, int i){
+   load_4x4(c1,c2,c3,c4,d,i);
+   load_4x4(c1,c2,c3,c4,d,i+16);
+}
+
+static inline void shuffle_fwd_8(vtf &c1, vtf &c2, vtf &c3, vtf &c4, vtf &c5, vtf &c6, vtf &c7, vtf &c8, double *d, int i){
+   c8 = c7;
+   c7 = c6;
+   c6 = c5;
+   c5 = c4;
+   c4 = c3;
+   c3 = c2;
+   c2 = c1;
+   c1 = aload(d,i);
+}
+
+
+template<typename dtype, typename vt>
+static inline void store_4(vt &c1, vt &c2, vt &c3, vt &c4, dtype *d, int i){
+   astore(c1,d,i);
+   astore(c2,d,i+4);
+   astore(c3,d,i+8);
+   astore(c4,d,i+12);
+}
+
+template<typename dtype, typename vt>
+static inline void store_8(vt &c1, vt &c2, vt &c3, vt &c4, vt &c5, vt &c6, vt &c7, vt &c8, dtype *d, int i){
+   store_4(c1,c2,c3,c4,d,i);
+   store_4(c5,c6,c7,c8,d,i+16);
+}
+
+
+static inline void shuffle_fwd_4(vtf &a, vtf &b, vtf &c, vtf &d, double*e, int i){
+   d = c;
+   c = b;
+   b = a;
+   a = aload(e,i);
+}
+
+
+static inline void fma_2(vtf &c1, vtf& c2, vtf &x1, vtf &x2, double *d, int i){
+   c1 = fma(x1,aload(d,i),c1);
+   c2 = fma(x2,aload(d,i+4),c2);
+}
+
+static inline void prod_2(vtf &c1, vtf& c2, vtf &x1, vtf &x2){
+   c1 *= x1;
+   c2 *= x2;
+}
+
+static inline void prod_2(vtf &c1, vtf& c2, double *d, int i){
+   c1 *= aload(d,i);
+   c2 *= aload(d,i+4);
+}
+
+static inline void load_2(vtf &c1, vtf &c2, double *d, int i){
+   c1 = aload(d,i);
+   c2 = aload(d,i+4);
+}
+
+static inline void shuffle_fwd_2(vtf &c1, vtf &c2, double*d, int i){
+   c2 = c1;
+   c1 = aload(d,i);
+}
+
+static inline void store_2(vtf &c1, vtf &c2, double *d, int i){
+   astore(c1,d,i);
+   astore(c2,d,i+4);
+}
+
+// with high register count variations, we should try using memory operands for s on both passes.
+
+static inline void xcorr_kern(double *cx, double *dx, double *df, double *s, double *buf, int diag, int offset, int n, int m){
+    vtf ca,cb,cc,cd,ce,cf,cg,ch;
+    load_8x1(ca,cb,cc,cd,ce,cf,cg,ch,cx,diag);
+    vtf xa, xb, xc, xd, xe, xf, xg, xh,
+        fa, fb, fc, fd, fe, ff, fg, fh;
+    load_8x1(xa,xb,xc,xd,xe,xf,xg,xh,dx,diag+offset);
+    load_8x1(fa,fb,fc,fd,fe,ff,fg,fh,df,diag+offset);
+    for(int k = 0; k < 256; k+= 4){
+        int p = offset+diag+k;
+        int q = offset+k;
+        fma_8x1(ca,cb,cc,cd,ce,cf,cg,ch,
+                dx,df,q,p);
+        fma_8x1(ca,cb,cc,cd,ce,cf,cg,ch,
+                df,dx,q,p);
+        vtf d1,d2,d3,d4,d5,d6,d7,d8;
+        load_8x1(d1,d2,d3,d4,d5,d6,d7,d8,s,q);
+        prod_8x1(ca,cb,cc,cd,ce,cf,cg,ch,d1,d2,d3,d4,d5,d6,d7,d8);
+        prod_8x1(d1,d2,d3,d4,d5,d6,d7,d8,s,p);
+        store_8(d1,d2,d3,d4,d5,d6,d7,d8,buf,offset+8*k);   
+        astore(ch,cx,diag+k);
+        shuffle_fwd_8(ca,cb,cc,cd,ce,cf,cg,ch,cx,diag+k);
+    }
+    store_8(ca,cb,cc,cd,ce,cf,cg,ch,cx,diag+128);
+}
+
+
+/*static inline void xcorr_kern(double *cx, double *dx, double *df, double *s, double *buf, int diag, int offset, int n, int m){
+    vtf ca,cb,cc,cd,ce,cf,cg,ch;
+    load_8x1(ca,cb,cc,cd,ce,cf,cg,ch,cx,diag);
+    vtf xa, xb, xc, xd, xe, xf, xg, xh,
+        fa, fb, fc, fd, fe, ff, fg, fh;
+    load_8x1(xa,xb,xc,xd,xe,xf,xg,xh,dx,diag+offset);
+    load_8x1(fa,fb,fc,fd,fe,ff,fg,fh,df,diag+offset);
+    for(int k = 0; k < 128; k+= 4){
+        int p = offset+diag+k;
+        int q = offset+k;
+        fma_8x1(ca,cb,cc,cd,ce,cf,cg,ch,
+                xa,xb,xc,xd,xe,xf,xg,xh,df,q);
+        fma_8x1(ca,cb,cc,cd,ce,cf,cg,ch,
+                fa,fb,fc,fd,fe,ff,fg,fh,dx,q);
+        vtf d1,d2,d3,d4,d5,d6,d7,d8;
+        load_8x1(d1,d2,d3,d4,d5,d6,d7,d8,s,q);
+        prod_8x1(ca,cb,cc,cd,ce,cf,cg,ch,d1,d2,d3,d4,d5,d6,d7,d8);
+        prod_8x1(d1,d2,d3,d4,d5,d6,d7,d8,s,p);
+        store_8(d1,d2,d3,d4,d5,d6,d7,d8,buf,offset+8*k);   
+        astore(ch,cx,diag+k);
+        shuffle_fwd_8(ca,cb,cc,cd,ce,cf,cg,ch,cx,diag+k);
+    }
+    store_8(ca,cb,cc,cd,ce,cf,cg,ch,cx,diag+128);
+}*/
+
+// there should be a separate reduce rows and reduce columns
+
+static void reduce_kern(double *a, double *output, long *outputi, int diag, int offset,int n, int m){
+    vtf m1, m2, m3, m4,
+        ca, cb, cc, cd;
+    vti i1, i2, i3, i4; 
+    load_4x4(m1,m2,m3,m4,output,offset);
+    load_4x4(i1,i2,i3,i4,outputi,offset);
+    for(int k = 0; k < 2048; k += 16){
+        load_4x4(ca,cb,cc,cd,a,offset+k);
+        max_4x4(m1,m2,m3,m4,ca,cb,cc,cd);
+        cmp_4x4(ca,cb,cc,cd,m1,m2,m3,m4);
+        seti_4x4(i1,i2,i3,i4,ca,cb,cc,cd,diag*4+offset*8*k);
+    }
+    store_4(m1,m2,m3,m4,output,offset);
+    store_4(i1,i2,i3,i4,outputi,offset);
+}
+
+void  accumTest4_7_10(double* cx,double* dx, double* df, double*s, double* a, double* output, long* outputi,int n, int m){
+   unsigned long t= 0;
+   int offset = 0;
+//   #pragma omp parallel for
+   for(int diag = 0; diag < n-32*m; diag += 32){
+      for(int offset = 0; offset < n-32*m; offset += 64){
+         xcorr_kern(cx,dx,df,s,a,diag,offset,n,m);
+         reduce_kern(a,output,outputi,diag,offset,n,m);
+      }
+   }     
+   printf("iterations: %lu\n",t);
+}
+
--- src/utils/arith.hpp
+++ src/utils/arith.hpp
@@ -1,5 +1,6 @@
 #include "../arch/avx2arith_2.hpp"
-
+//#include <cmath>
+#include "incfused.hpp"
 // Collection of in place 2 operand arithmetic and sliding window sum and average. These are based on 
 // ACCURATE SUM AND DOT PRODUCT Ogita et al
 
@@ -8,15 +9,6 @@ using namespace vmth;
 #define fact 2 << 27 
 
 
-// not sure if it's legal to use an enumerated type in second arg
-template<typename dtype>
-static inline double factor(dtype a){
-   // this needs to be made compliant with vector types, perhaps by overloading set(scalar) for scalar operands
-   //dtype b = static_cast<dtype>(a);
-   static const dtype c = bcast(static_cast<dtype>(fact));
-   return a*c;
-//   return _mm256_castsi256_pd(bcast(fact))*a;
-}
 
 template<typename dtype>
 static inline void xadd(dtype &a, dtype &b){
@@ -43,14 +35,11 @@ static inline void xmul(dtype &a,dtype &b){
    b = fms(a,b,c);
    a = c;
 #else
-   dtype c = a;
-   a *= b;
-   dtype f = b;
-   dtype d;
-   dtype g;
-   xsplit(c,d);
-   xsplit(f,g);
-   b = d*g - (((a - c*f) - c*f) - c*g);
+   // compiler didn't define it, go go libc
+   dtype c = a*b;
+//   b = ma(a,b,-1*c);
+//   b = fma(a,b,-1*c);
+   a = c;
 #endif
 }
 
@@ -72,7 +61,6 @@ static inline vtype xsInv_kern(dtype *a, dtype *mu, dtype *sI, int offset, long 
    xmul(p,s);
    dtype u = static_cast<dtype>(winlen);
    vtype t = bcast(u);
- //  ustore(t,sI,0);
    for(int j = offset+1; j < offset+winlen; j++){
        vtype h = uload(a,j) - z;
        vtype r = h;
@@ -102,6 +90,19 @@ void xsInv(dtype *a, dtype *mu, dtype *sI, int len, int winlen){
 //   }
 }
 
+template<typename dtype>
+void sInv(dtype *a, dtype *mu, dtype *sI, int len, int winlen){
+   int mlen = len - winlen + 1;
+   #pragma omp parallel for
+   for(int i = 0; i < winlen; i++){
+     dtype s = uload(a,i);
+     dtype t = uload(a,i+4);
+     dtype ma = aload(mu,0);
+     dtype mb = aload(mu,4);
+   //  for(int i = 0; i < 
+   }
+}
+
 
 template<typename dtype>
 void xmean_windowed(dtype *a, dtype *output, int len, int winlen){
@@ -113,7 +114,6 @@ void xmean_windowed(dtype *a, dtype *output, int len, int winlen){
    for (int i = winlen; i < len; i++){
       dtype b = -1*a[i-winlen];
       xadd(s,b);
-     // xsub(s,b);
       e += b; 
       b = a[i];
       xadd(s,b);
