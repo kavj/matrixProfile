1130f885365c9d8fd63c785f02fb5e5c351813fa
kavj <kjkamgar@gmail.com>
2018-01-31 04:17:43 -0800

    ...sigh

--- src/kernel/refpwed.cpp
+++ src/kernel/refpwed.cpp
@@ -3,7 +3,8 @@
 
 #include "refpwed.h" 
 
-/*
+
+
 void mpkern(double* __restrict__ cx, const double* __restrict__ dx, const double* __restrict__ xf, double* __restrict__ s, double* __restrict__ buf, int diag, int offset){
    cx = (double*)__builtin_assume_aligned(cx,64);
    dx = (double*)__builtin_assume_aligned(dx,64);
@@ -11,23 +12,26 @@ void mpkern(double* __restrict__ cx, const double* __restrict__ dx, const double
    s  = (double*)__builtin_assume_aligned(s,64);
    buf =(double*)__builtin_assume_aligned(buf,64);
    
-   const int tilesz = 512;
+   const int tilesz = 32;
    int m = diag+offset;
-   for(int j = diag; j < diag+tilesz; j++){
-      for(int i = offset; i < tilesz; i++){
+   for(int i = offset; i < offset+tilesz; i++){
+      double s1 = s[i];
+      double dxi = dx[i];
+      double xfi = xf[i];      
+      for(int j = diag; j < diag+tilesz; j++){
          int k = i+j;
          double c = cx[i];
-         double s1 = s[i];
-         c = c + dx[i]*xf[k];
-         c = c + dx[k]*xf[i];
-         double s2 = s[i];
-         double d = c*s1*s2;
+         c += dxi*xf[k];
+         c += dx[k]*xfi;
+         cx[j] = c;
+         double d = c*s1;
+         d *= s[k];
          buf[k-offset] = d;
       }
    }
 }
-*/
 
+/*
 void mpkern2(double* __restrict__ cx, const double* __restrict__ dx, const double* __restrict__ xf, double* __restrict__ s, double* __restrict__ buf, int diag, int offset){
    cx = (double*)__builtin_assume_aligned(cx,64);
    dx = (double*)__builtin_assume_aligned(dx,64);
@@ -36,10 +40,10 @@ void mpkern2(double* __restrict__ cx, const double* __restrict__ dx, const doubl
    buf =(double*)__builtin_assume_aligned(buf,64);
    
    const int tilesz = 512;
-   int m = diag+offset;
    for(int j = diag; j < diag+tilesz; j++){
       for(int i = offset; i < offset+tilesz; i+=10){
          int k = i+j;
+         int m = k-offset;
          double ca = cx[i];
          double cb = cx[i+1];
          double cc = cx[i+2];
@@ -50,6 +54,7 @@ void mpkern2(double* __restrict__ cx, const double* __restrict__ dx, const doubl
          double ch = cx[i+7];
          double ci = cx[i+8];
          double cj = cx[i+9];
+
          double bcst = dx[i]; 
 
          ca += bcst*xf[k];
@@ -111,21 +116,19 @@ void mpkern2(double* __restrict__ cx, const double* __restrict__ dx, const doubl
          ci *= s[k+8];
          cj *= s[k+9];
 
-         buf[k-offset] = ca;
-         buf[k-offset+1] = cb;
-         buf[k-offset+2] = cc;
-         buf[k-offset+3] = cd;
-         buf[k-offset+4] = ce;
-         buf[k-offset+5] = cf;
-         buf[k-offset+6] = cg;
-         buf[k-offset+7] = ch;
-         buf[k-offset+8] = ci;
-         buf[k-offset+9] = cj;
+         buf[m] = ca;
+         buf[m+1] = cb;
+         buf[m+2] = cc;
+         buf[m+3] = cd;
+         buf[m+4] = ce;
+         buf[m+5] = cf;
+         buf[m+6] = cg;
+         buf[m+7] = ch;
+         buf[m+8] = ci;
+         buf[m+9] = cj;
 
       }
    }
-}
-
-
+}*/
 
 
