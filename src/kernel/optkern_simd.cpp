88e4ce92507644bb1af0287fd844e8d6d5ffba73
kavj <kjkamgar@gmail.com>
2018-02-02 11:52:33 -0800

    backup of changes

--- src/kernel/optkern_simd.cpp
+++ src/kernel/optkern_simd.cpp
@@ -1,120 +1,160 @@
 #include<cstdio>
-#include "../arch/v256.h"
-#include "../utils/xprec_math.hpp"
+//#include "v256.h"
+#include "../arch/avx2arith_2.hpp"
+//#include "../utils/xprec_math.hpp"
 
-/*
-template<typename vt>
-static inline void xcorr_reduce_horizontal(double* __restrict__ buf, double* __restrict__ prof, long* __restrict__ profi, int diag, int offset){
-   int stride = 4;
-   vt ca = aload(buf,offset);
-   vt cb = aload(buf,offset+stride);
-   vt cc = aload(buf,offset+2*stride);
-   vt cd = aload(buf,offset+3*stride);
-   vt ce = aload(buf,offset+4*stride);
-   vt cf = aload(buf,offset+5*stride);
-//   for(int i = 0; i < tilesz; i++){
-      
-//   }
-}*/
+// temporary 
+typedef  __m256d vt;
+using namespace vmth;
 
-static inline void xcorr_reduce_vertical(double* __restrict__ buf, double* __restrict__ prof, long* __restrict__ profi, int diag, int offset){
-   
+//template<typename vt>
+static inline void xcorr_2(vt &ca, vt &cb, vt &cc, vt &cd, vt &ce, vt &cf, vt &cg, vt &ch, double* __restrict__ dx, double* __restrict__ xf, int diag, int offset) __attribute__ ((always_inline));
 
-}
+//template<typename vt>
 
-
-static inline void xcorr_2(vt &ca, vt &cb, vt &cc, vt &cd, vt &ce, vt &cf, vt &cg, vt &ch, double* __restrict__ dx, double* __restrict__ xf, double* __restrict__ s, double*  __restrict__ output, int diag, int k) __attribute__ ((always_inline));
-
-template<typename vt>
-static inline void xcorr_2(vt &ca, vt &cb, vt &cc, vt &cd, vt &ce, vt &cf, vt &cg, vt &ch, double *dx, double *xf, double *s, double *buf, int diag, int offset){
+static inline void xcorr_2(vt &ca, vt &cb, vt &cc, vt &cd, vt &ce, vt &cf, vt &cg, vt &ch, double *dx, double *xf, int diag, int offset){
    int p = diag+offset;
    int stride = 4;
+
    vt fbc = bcast(dx,offset);
    
-   ca = vfma(fbc,aload(xf,p),ca);
-   cb = vfma(fbc,aload(xf,p+stride),cb);
-   cc = vfma(fbc,aload(xf,p+2*stride),cc);
-   cd = vfma(fbc,aload(xf,p+3*stride),cd);
-   ce = vfma(fbc,aload(xf,p+4*stride),ce);
-   cf = vfma(fbc,aload(xf,p+5*stride),cf);
-   cg = vfma(fbc,aload(xf,p+6*stride),cg);
-   ch = vfma(fbc,aload(xf,p+7*stride),ch);
+   ca = fma(fbc,aload(xf,p),ca);
+   cb = fma(fbc,aload(xf,p+stride),cb);
+   cc = fma(fbc,aload(xf,p+2*stride),cc);
+   cd = fma(fbc,aload(xf,p+3*stride),cd);
+   ce = fma(fbc,aload(xf,p+4*stride),ce);
+   cf = fma(fbc,aload(xf,p+5*stride),cf);
+   cg = fma(fbc,aload(xf,p+6*stride),cg);
+   ch = fma(fbc,aload(xf,p+7*stride),ch);
 
-   fbc = bcast(xf,offset); 
+   fbc = bcast(xf,offset);   
+
+   ca = fma(fbc,aload(dx,p),ca);
+   cb = fma(fbc,aload(dx,p+stride),cb);
+   cc = fma(fbc,aload(dx,p+2*stride),cc);
+   cd = fma(fbc,aload(dx,p+3*stride),cd);
+   ce = fma(fbc,aload(dx,p+4*stride),ce);
+   cf = fma(fbc,aload(dx,p+5*stride),cf);
+   cg = fma(fbc,aload(dx,p+6*stride),cg);
+   ch = fma(fbc,aload(dx,p+7*stride),ch);
+
+
+}
+typedef __m256i vi;
+static inline void xcorr_reduce(vt ca, vt cb, vt cc, vt cd, vt ce, vt cf, vt cg, vt ch, double *s, double *output, long *outputi, int diag, int offset){
+   const int stride = 4;
+   const int p = diag+offset;
+   const int q = 4*p; 
+   const int r = 4*offset;
    
-   ca = vfma(fbc,aload(dx,p),ca);
-   cb = vfma(fbc,aload(dx,p+stride),cb);
-   cc = vfma(fbc,aload(dx,p+2*stride),cc);
-   cd = vfma(fbc,aload(dx,p+3*stride),cd);
-   ce = vfma(fbc,aload(dx,p+4*stride),ce);
-   cf = vfma(fbc,aload(dx,p+5*stride),cf);
-   cg = vfma(fbc,aload(dx,p+6*stride),cg);
-   ch = vfma(fbc,aload(dx,p+7*stride),ch);
-
-   vt da, db, dc, dd, de, df, dg, dh;
-   fbc = bcast(s,offset);
-   da = ca * fbc;
-   db = cb * fbc;
-   dc = cc * fbc;
-   dd = cd * fbc;
-   de = ce * fbc;
-   df = cf * fbc;
-   dg = cg * fbc;
-   dh = ch * fbc;
-
-   da *= aload(s,p);
-   db *= aload(s,p+stride);
-   dc *= aload(s,p+2*stride);
-   dd *= aload(s,p+3*stride);
-   de *= aload(s,p+4*stride);
-   df *= aload(s,p+5*stride);
-   dg *= aload(s,p+6*stride);
-   dh *= aload(s,p+7*stride);
+   ca *= aload(s,p);
+   cb *= aload(s,p+stride);
+   cc *= aload(s,p+2*stride);
+   cd *= aload(s,p+3*stride);
+   ce *= aload(s,p+4*stride);
+   cf *= aload(s,p+5*stride);
+   cg *= aload(s,p+6*stride);
+   ch *= aload(s,p+7*stride);
   
-   int k = 4*p;
+   vt fbc = bcast(s,offset);
+   ca = ca * fbc;
+   cb = cb * fbc;
+   cc = cc * fbc;
+   cd = cd * fbc;
+   ce = ce * fbc;
+   cf = cf * fbc;
+   cg = cg * fbc;
+   ch = ch * fbc;
 
-   strided_store_x8<vt,double,4>(da,db,dc,dd,de,df,dg,dh,buf,4*p);
+   vt da, db, dc, dd;
+   vi ga, gb, gc, gd;
+
+   const vi four = bcast(4);
+   const vi st   = set(0,1,2,3);
+   vi st2 = st + bcast(offset);
+   vi st3 = st2 + four;
+   
+   da = cb < ca;
+   db = cc < cd;
+   dc = ce < cf;
+   dd = cg < ch;
+
+   ga = blend(st2,st3,da);
+   st2 = st3;
+   st3 = st3 + four;
+   gb = blend(st2,st3,db);
+   st2 = st3;
+   st3 = st3 + four;
+   gc = blend(st2,st3,dc);
+   st2 = st3;
+   st3 = st3 + four;
+   gd = blend(st2,st3,dd);
+
+   ca = max(ca,cb);
+   cc = max(cc,cd);
+   cd = max(ce,cf);
+   cg = max(cg,ch);
+
+   cb = cc < ca;
+   ce = cg < cd;
+
+   ga = blend(ga,gb,cb);
+   gc = blend(gc,gd,ce);
+   
+    
+   
+
+/*   ca = max(da,db);
+   cc = max(dc,dd);
+   ce = max(de,df);
+   cg = max(dg,dh);
+
+   ca = max(ca,cc);
+   ce = max(ce,cg);
+   ca = max(ca,ce);
+   astore(ca,output,r);
+
+   da = max(da,aload(output,q));
+   db = max(db,aload(output,q+stride));
+   dc = max(dc,aload(output,q+2*stride));
+   dd = max(dd,aload(output,q+3*stride));
+   de = max(de,aload(output,q+4*stride));
+   df = max(da,aload(output,q+5*stride));
+   dg = max(db,aload(output,q+6*stride));
+   dh = max(dc,aload(output,q+7*stride));
  
+   strided_store_x8<vt,4>(da,db,dc,dd,de,df,dg,dh,output,q 
+*/
 }
 
 
 
+static inline void xcorr_kern(double* __restrict__ cx, const double* __restrict__ dx, double* __restrict__ xf, double* __restrict__ s, double* __restrict__ output, long* __restrict__ outputi, int diag, int offset) __attribute__ ((always_inline));
 
-
-static inline void xcorr_reduce(double* __restrict__ buf, double* __restrict__ output, long*  __restrict__ outputi, int diag, int k) __attribute__ ((always_inline));
-
-static inline void xcorr_reduce(double* __restrict__ buf, double* __restrict__ output, long*  __restrict__ outputi, int diag, int k){
- // for(int i = 0; i < 32 
-
-}
-
-template<typename vt>
-static inline void xcorr_kern(double* __restrict__ cx, const double* __restrict__ dx, double* __restrict__ xf, double* __restrict__ s, double* __restrict__ buf, int diag, int offset) __attribute__ ((always_inline));
-
-template<typename vt>
-static inline void xcorr_kern(double* __restrict__ cx, double* __restrict__ dx, double* __restrict__ xf, double* __restrict__ s, double* __restrict__ buf, int diag, int offset){
+static inline void xcorr_kern(double* __restrict__ cx, double* __restrict__ dx, double* __restrict__ xf, double* __restrict__ s, double* __restrict__ output, long* __restrict__ outputi, int diag, int offset){
    for(int subdiag = diag; subdiag < diag + 2048; subdiag += 32){
       vt ca, cb, cc, cd, ce, cf, cg, ch;
-     // strided_load_x8<vt,double,4>(ca,cb,cc,cd,ce,cf,cg,ch,cx,subdiag);
-      
+      strided_load_x8<vt,4>(ca,cb,cc,cd,ce,cf,cg,ch,cx,subdiag);
       for(int suboff = offset; suboff < offset+2048; suboff+=4){
-         xcorr_2 <__m256d>(ca,cb,cc,cd,ce,cf,cg,ch, dx, xf, s, buf, subdiag, suboff);
+         xcorr_2(ca,cb,cc,cd,ce,cf,cg,ch,dx,xf,subdiag,suboff);
+         xcorr_reduce(ca,cb,cc,cd,ce,cf,cg,ch,s,output,outputi,subdiag,suboff);
       }
-     // strided_store_x8<vt,double,4>(ca,cb,cc,cd,ce,cf,cg,ch,cx,subdiag);
+      strided_store_x8<vt,4>(ca,cb,cc,cd,ce,cf,cg,ch,cx,subdiag);
    }
 }
 
 //#define tilesz 2048
-template<typename dtype, typename itype>
-void  accumTest4_7_10(dtype* cx,dtype* dx, dtype* xf, dtype*s, dtype* buf, dtype* output, itype* outputi,int n, int m){
+//template<typename dtype, typename itype>
+void  accumTest4_7_10(double* cx,double* dx, double* xf, double*s, double* buf, double* output, long* outputi,int n, int m){
    unsigned long t= 0;
    const int tilesz = 2048;
    const int stride = 4;
+   
    #pragma omp parallel for  
    for(int diag = 0; diag < n-16*m; diag += 2048){
       for(int offset = 0; offset < n-diag-16*m; offset+=2048){
          t ++;
-         xcorr_kern(cx,dx,xf,s,buf,diag,offset);
+         xcorr_kern(cx,dx,xf,s,output,outputi,diag,offset);
       }
    }   
    printf("iterations: %lu \n",t);
--- src/kernel/refpwed.cpp
+++ src/kernel/refpwed.cpp
@@ -1,134 +1,249 @@
+#include<cstdio>
+#include<cmath>
+#include <algorithm>
+//#include "refpwed.h" 
+#define tilesz 128 
+#define largetile 512 
+using namespace std;
 
+static inline void mpkern(double* __restrict__ cx, const double*  __restrict__ dx, const double* __restrict__ xf, const double* __restrict__ s, double* __restrict__ buf, int diag, int offset) __attribute__ ((always_inline));
 
-
-#include "refpwed.h" 
-
-
-
-void mpkern(double* __restrict__ cx, const double* __restrict__ dx, const double* __restrict__ xf, double* __restrict__ s, double* __restrict__ buf, int diag, int offset){
+/*
+static inline void mpkern(double* __restrict__ cx, const double* __restrict__ dx, const double* __restrict__ xf, double* __restrict__ s, double* __restrict__ mp, long* __restrict__ mpi, int diag, int offset){
    cx = (double*)__builtin_assume_aligned(cx,64);
    dx = (double*)__builtin_assume_aligned(dx,64);
    xf = (double*)__builtin_assume_aligned(xf,64);
    s  = (double*)__builtin_assume_aligned(s,64);
-   buf =(double*)__builtin_assume_aligned(buf,64);
+   mp =(double*)__builtin_assume_aligned(mp,64);
    
-   const int tilesz = 32;
-   int m = diag+offset;
-   for(int i = offset; i < offset+tilesz; i++){
-      double s1 = s[i];
-      double dxi = dx[i];
-      double xfi = xf[i];      
-      for(int j = diag; j < diag+tilesz; j++){
+   const int m = diag+offset;
+   for(int i = diag; i < offset+tilesz; i+=8){
+      double r1 = cx[i];
+      double r2 = cx[i+1];
+      double r3 = cx[i+2];
+      double r4 = cx[i+3];
+      double r5 = cx[i+4];
+      double r6 = cx[i+5];
+      double r7 = cx[i+6];
+      double r8 = cx[i+7];       
+      for(int j = offset; j < diag+tilesz; j++){
          int k = i+j;
-         double c = cx[i];
-         c += dxi*xf[k];
-         c += dx[k]*xfi;
-         cx[j] = c;
-         double d = c*s1;
-         d *= s[k];
-         buf[k-offset] = d;
+         double dxi = dx[i];
+         r1 = fma(dxi,xf[k],r1);
+         r2 = fma(dxi,xf[k+1],r2);
+         r3 = fma(dxi,xf[k+2],r3);
+         r4 = fma(dxi,xf[k+3],r4);
+         r5 = fma(dxi,xf[k+4],r5);
+         r6 = fma(dxi,xf[k+5],r6);
+         r7 = fma(dxi,xf[k+6],r7);
+         r8 = fma(dxi,xf[k+7],r8);
+
+         dxi = xf[i];
+         r1 = fma(dxi,dx[k],r1);
+         r2 = fma(dxi,dx[k+1],r2);
+         r3 = fma(dxi,dx[k+2],r3);
+         r4 = fma(dxi,dx[k+3],r4);
+         r5 = fma(dxi,dx[k+4],r5);
+         r6 = fma(dxi,dx[k+5],r6);
+         r7 = fma(dxi,dx[k+6],r7);
+         r8 = fma(dxi,dx[k+7],r8);
+
+         double t1 = r1*s[k];
+         double t2 = r2*s[k+1];
+         double t3 = r3*s[k+2];
+         double t4 = r4*s[k+3];
+         double t5 = r1*s[k+4];
+         double t6 = r2*s[k+5];
+         double t7 = r3*s[k+6];
+         double t8 = r4*s[k+7];
+
+         t1 = r1*s[k];
+         t2 = r2*s[k+1];
+         t3 = r3*s[k+2];
+         t4 = r4*s[k+3];
+         t5 = r1*s[k+4];
+         t6 = r2*s[k+5];
+         t7 = r3*s[k+6];
+         t8 = r4*s[k+7];
+        
+         mp[k] = max(t1,mp[k]);
+         mp[k+1] = max(t2,mp[k+1]);
+         mp[k+2] = max(t3,mp[k+2]);
+         mp[k+3] = max(t4,mp[k+3]);
+         mp[k+4] = max(t5,mp[k+4]);
+         mp[k+5] = max(t6,mp[k+5]);
+         mp[k+6] = max(t7,mp[k+6]);
+         mp[k+7] = max(t8,mp[k+7]);
+
+         t1 = max(t1,t2);
+         t3 = max(t3,t4);
+         t5 = max(t5,t6);
+         t7 = max(t7,t8);
+         t1 = max(t1,t3);
+         t5 = max(t5,t7);
+         t1 = max(t1,mp[i]);
+         mp[i] = max(t1,t5);
+      }
+   }
+}*/
+
+
+
+
+static inline void mpkern(double* __restrict__ cx, const double* __restrict__ dx, const double* __restrict__ xf, double* __restrict__ s, double* __restrict__ mp, long* __restrict__ mpi, int diag, int offset){
+   cx = (double*)__builtin_assume_aligned(cx,64);
+   dx = (double*)__builtin_assume_aligned(dx,64);
+   xf = (double*)__builtin_assume_aligned(xf,64);
+   s  = (double*)__builtin_assume_aligned(s,64);
+   mp =(double*)__builtin_assume_aligned(mp,64);
+   
+   const int m = diag+offset;
+   for(int i = diag; i < offset+tilesz; i++){
+      double r1 = cx[i];
+      double r2 = cx[i+1];
+      double r3 = cx[i+2];
+      double r4 = cx[i+3];
+      double r5 = cx[i+4];
+      double r6 = cx[i+5];
+      double r7 = cx[i+6];
+      double r8 = cx[i+7];       
+      for(int j = offset; j < diag+tilesz; j++){
+         int k = i+j;
+         double dxi = dx[i];
+         r1 = fma(dxi,xf[k],r1);
+         r2 = fma(dxi,xf[k+1],r2);
+         r3 = fma(dxi,xf[k+2],r3);
+         r4 = fma(dxi,xf[k+3],r4);
+         r5 = fma(dxi,xf[k+4],r5);
+         r6 = fma(dxi,xf[k+5],r6);
+         r7 = fma(dxi,xf[k+6],r7);
+         r8 = fma(dxi,xf[k+7],r8);
+
+         dxi = xf[i];
+         r1 = fma(dxi,dx[k],r1);
+         r2 = fma(dxi,dx[k+1],r2);
+         r3 = fma(dxi,dx[k+2],r3);
+         r4 = fma(dxi,dx[k+3],r4);
+         r5 = fma(dxi,dx[k+4],r5);
+         r6 = fma(dxi,dx[k+5],r6);
+         r7 = fma(dxi,dx[k+6],r7);
+         r8 = fma(dxi,dx[k+7],r8);
+
+         double t1 = r1*s[k];
+         double t2 = r2*s[k+1];
+         double t3 = r3*s[k+2];
+         double t4 = r4*s[k+3];
+         double t5 = r1*s[k+4];
+         double t6 = r2*s[k+5];
+         double t7 = r3*s[k+6];
+         double t8 = r4*s[k+7];
+
+         t1 = r1*s[k];
+         t2 = r2*s[k+1];
+         t3 = r3*s[k+2];
+         t4 = r4*s[k+3];
+         t5 = r1*s[k+4];
+         t6 = r2*s[k+5];
+         t7 = r3*s[k+6];
+         t8 = r4*s[k+7];
+        
+         if(mp[k] < t1){
+            mp[k] = t1;
+            mpi[i] = k;
+         }
+         if(mp[k+1] < t2){
+            mp[k+1] = t2;
+            mpi[k+1] = j;
+         }
+         if(mp[k+2] < t3){
+            mp[k+2] = t3;
+            mpi[k+2] = j;
+         }
+         if(mp[k+3] < t4){
+            mp[k+3] = t4;
+            mpi[k+3] = j;
+         }
+         if(mp[k+4] < t5){
+            mp[k+4] = t5;
+            mp[k+4] = j;
+         }
+         if(mp[k+5] < t6){
+            mp[k+5] = t6;
+            mpi[k+5] = j;
+         }
+         if(mp[k+6] < t7){
+            mp[k+6] = t7;
+            mpi[k+6] = j;
+         }
+         if(mp[k+7] < t8){
+            mp[k+7] = t8; 
+            mpi[k+7] = j;
+         }
+         k -= m;
+
+         int col1 = offset;
+         int col2 = offset+2;
+         int col3 = offset+4;
+         int col4 = offset+6;
+
+         if(t1 < t2){
+            t1 = t2;
+            col1++;
+         }
+         if(t3 < t4){
+            t3 = t4;
+            col2++;
+         }
+         if(t5 < t6){
+            t5 = t6;
+            col3++;
+         }
+         if(t7 < t8){
+            t7 = t8;
+            col4++;
+         }
+         if(t1 < t3){
+            t1 = t3;
+            col1 = col2;
+         }
+         if(t5 < t7){
+            t5 = t7;
+            col3 = col4;
+         }
+         if(t1 < t5){
+            t1 = t5;
+            col1 = col3;
+         }
+         if(t1 < mp[offset]){
+            mp[offset] = t1;
+            mpi[offset] = col1;
+         }
+      
       }
    }
 }
 
-/*
-void mpkern2(double* __restrict__ cx, const double* __restrict__ dx, const double* __restrict__ xf, double* __restrict__ s, double* __restrict__ buf, int diag, int offset){
-   cx = (double*)__builtin_assume_aligned(cx,64);
-   dx = (double*)__builtin_assume_aligned(dx,64);
-   xf = (double*)__builtin_assume_aligned(xf,64);
-   s  = (double*)__builtin_assume_aligned(s,64);
-   buf =(double*)__builtin_assume_aligned(buf,64);
-   
-   const int tilesz = 512;
-   for(int j = diag; j < diag+tilesz; j++){
-      for(int i = offset; i < offset+tilesz; i+=10){
-         int k = i+j;
-         int m = k-offset;
-         double ca = cx[i];
-         double cb = cx[i+1];
-         double cc = cx[i+2];
-         double cd = cx[i+3];
-         double ce = cx[i+4];
-         double cf = cx[i+5];
-         double cg = cx[i+6];
-         double ch = cx[i+7];
-         double ci = cx[i+8];
-         double cj = cx[i+9];
 
-         double bcst = dx[i]; 
-
-         ca += bcst*xf[k];
-         cb += bcst*xf[k+1];
-         cc += bcst*xf[k+2];
-         cd += bcst*xf[k+3];
-         ce += bcst*xf[k+4];
-         cf += bcst*xf[k+5];
-         cg += bcst*xf[k+6];
-         ch += bcst*xf[k+7];
-         ci += bcst*xf[k+8];
-         cj += bcst*xf[k+9]; 
- 
-         bcst = xf[i];
-
-         ca += bcst*dx[k];
-         cb += bcst*dx[k+1];
-         cc += bcst*dx[k+2];
-         cd += bcst*dx[k+3];
-         ce += bcst*dx[k+4];
-         cf += bcst*dx[k+5];
-         cg += bcst*dx[k+6];
-         ch += bcst*dx[k+7];
-         ci += bcst*dx[k+8];
-         cj += bcst*dx[k+9];
-
-         cx[i] = ca;
-         cx[i+1] = cb;
-         cx[i+2] = cc;
-         cx[i+3] = cd;
-         cx[i+4] = ce;
-         cx[i+5] = cf;
-         cx[i+6] = cg;
-         cx[i+7] = ch; 
-         cx[i+8] = ci;
-         cx[i+9] = cj;
-
-         bcst = s[i]; 
-
-         ca *= bcst;
-         cb *= bcst;
-         cc *= bcst;
-         cd *= bcst;
-         ce *= bcst;
-         cf *= bcst;
-         cg *= bcst;
-         ch *= bcst;
-         ci *= bcst;
-         cj *= bcst;
-         
-         ca *= s[k];
-         cb *= s[k+1];
-         cc *= s[k+2];
-         cd *= s[k+3];
-         ce *= s[k+4];
-         cf *= s[k+5];
-         cg *= s[k+6];
-         ch *= s[k+7];
-         ci *= s[k+8];
-         cj *= s[k+9];
-
-         buf[m] = ca;
-         buf[m+1] = cb;
-         buf[m+2] = cc;
-         buf[m+3] = cd;
-         buf[m+4] = ce;
-         buf[m+5] = cf;
-         buf[m+6] = cg;
-         buf[m+7] = ch;
-         buf[m+8] = ci;
-         buf[m+9] = cj;
-
+static inline void xcorr_kern(double* __restrict__ cx, double* __restrict__ dx, double* __restrict__ xf, double* __restrict__ s, double* __restrict__ mp, long* __restrict__ mpi, int diag, int offset){
+   for(int subdiag = diag; subdiag < diag + largetile; subdiag += tilesz){
+      for(int suboff = offset; suboff < offset+largetile; suboff+= tilesz){
+         mpkern(cx, dx, xf, s, mp, mpi, diag, offset);
       }
    }
-}*/
+}
+
+//template<typename dtype, typename itype>
+void  accumTest4_7_10(double* cx,double* dx, double* xf, double*s, double* buf, double* output, long* outputi,int n, int m){
+   unsigned long t= 0;
+   #pragma omp parallel for  
+   for(int diag = 0; diag < n-16*m; diag += largetile){
+      for(int offset = 0; offset < n-diag-16*m; offset+=largetile){
+         //mpkern(cx, dx, xf, s, output, outputi, diag, offset);
+         xcorr_kern(cx,dx,xf,s,output,outputi,diag,offset);
+         t+= largetile*largetile;
+      }
+   }   
+   printf("iterations: %lu \n",t);
+}
 
 
