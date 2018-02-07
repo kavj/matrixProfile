65e7f903b9440bc6307527c96c604d7dbe75c616
kavj <kjkamgar@gmail.com>
2018-02-07 09:17:50 -0800

    bug fixes reference

--- src/kernel/reference.cpp
+++ src/kernel/reference.cpp
@@ -8,12 +8,12 @@ typedef long dti;
 // It could also be optimized to do sets of 8
 
 //template<typename dt>
-void initcov(dt *a, dt *mu, dt *invn, dt *c, dt *mp, dti *mpi, int mindiag, int len, int subLen){
+void initcov(dt *a, dt *cx, dt *mu, dt *invn, dt *mp, dti *mpi, int mindiag, int len, int subLen){
    dt m1 = mu[0];
    dt s1 = invn[0];
    dt maxmp = -1;
    dti maxmpi = -1;
-   int upperlim = len - subLen;
+   int upperlim = len - subLen + 1;
    for(int diag = mindiag; diag < upperlim; diag++){
       dt cov = 0;
       dt m2 = mu[diag];
@@ -22,11 +22,10 @@ void initcov(dt *a, dt *mu, dt *invn, dt *c, dt *mp, dti *mpi, int mindiag, int 
          dt c2 = a[diag+offset] - m2;
          cov = fma(c1,c2,cov);
       }
-      c[diag] = cov;
+      cx[diag] = cov;
       dt cor = cov * (s1 * invn[diag]);
       mp[diag] = cor;
-      mpi[diag] = 1;
-      printf("cov: %lf, cor %lf diag %d\n",cov,cor,diag);
+      mpi[diag] = 0;
       if(maxmp < cor){
          maxmp = cor;
          maxmpi = diag;
@@ -39,8 +38,22 @@ void initcov(dt *a, dt *mu, dt *invn, dt *c, dt *mp, dti *mpi, int mindiag, int 
 
 //solvempref(T,cx,mu,dF,dX,sigmaInv,buffer,bufferI,n,m,m);
 
+
+
+
 void solvempref(dt *a, dt *cx, dt *mu, dt *df, dt *dx, dt *s, dt *mp, dti *mpi, int len, int diagmin, int sublen){
+   cx = (dt*)__builtin_assume_aligned(cx,64);
+   dx = (dt*)__builtin_assume_aligned(dx,64);
+   df = (dt*)__builtin_assume_aligned(df,64);
+   s  = (dt*)__builtin_assume_aligned(s,64);
+   mp =(dt*)__builtin_assume_aligned(mp,64);
+   mpi = (dti*)__builtin_assume_aligned(mpi,64);
+
    initcov(a,cx,mu,s,mp,mpi,diagmin,len,sublen);
+/*   for(int i = 0; i < 10000; i+=100){
+      printf("%lf %d \n",cx[i],i);
+   }
+*/
    for(int i = diagmin; i < len-sublen; i++){
       int jmax = len - sublen - i + 1;
       dt cov = cx[i];
@@ -52,9 +65,6 @@ void solvempref(dt *a, dt *cx, dt *mu, dt *df, dt *dx, dt *s, dt *mp, dti *mpi, 
          cor *= s[k];
          dt mpa = mp[j];
          dt mpb = mp[k];
-         if(j < diagmin){
-            printf("%lf %lf %d\n",mpa,cor,j);
-         }
          if(mpa < cor){
             mp[j] = cor;
             mpi[j] = k;
