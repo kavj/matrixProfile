899299ba05168635961fb42b41e907e3f6142a9d
kavj <kjkamgar@gmail.com>
2018-04-16 04:43:12 -0700

    adjusting pscr

--- src/kernel/scrpp.cpp
+++ src/kernel/scrpp.cpp
@@ -27,16 +27,18 @@
 
 // It could also make sense to use an MKL back end? This way we could just set up xcorr directly
 
-void batch_normalize(const double* __Restrict__ ts, const double* __Restrict__ q, const double* __Restrict__ mu, int qstart, int qcount, int sublen, int step){
+// We center about the mean and interleave
+
+void batch_normalize(double* __Restrict__ qbuf,  const double* __Restrict__ ts, const double* __Restrict__ mu, int qstart, int qcount, int sublen, int step){
    int back = len-sublen+1;
-   block<double> reg;
+   block<double> qm;
    // hoist means 
    for(int i = 0; i < qcount; i++){
-      reg(i) = mu[i*step+qstart];
+      qm(i) = mu[i*step+qstart];
    } 
    for(int i = 0; i < sublen; i++){
       for(int j = 0; j < qcount; j++){
-         q[i*qcount+j] = ts[i+qstart+j*step]-reg(j);
+         q[i*qcount+j] = ts[i+qstart+j*step]-qm(j);
       }
    }
 }
@@ -48,39 +50,42 @@ void batch_normalize(const double* __Restrict__ ts, const double* __Restrict__ q
 // In general these things are spaced such that we shouldn't have a lot of things in there simultaneously
 // In fact I should just fall back to a reference if it's too narrow
 
+// it would be best to do this using an upper triangular pattern. It avoids a lot of nonsense
 
-
-static inline void partial_xcorr_kern(double* __Restrict__ qmcov, double* __Restrict__ qcorr, double* __Restrict__ qbuf, int64_t* __Restrict__ qind, const double* __Restrict__ ts, const double* __Restrict__ mu, const double* __Restrict__ invn, int qstart, int unroll, int sublen, int qstart, int step, int excl){
+static inline void partial_xcorr_kern(double* __Restrict__ qmcov, double* __Restrict__ corr, double* __Restrict__ qbuf, int64_t* __Restrict__ ind, const double* __Restrict__ ts, const double* __Restrict__ mu, const double* __Restrict__ invn, int qstart, int unroll, int sublen, int qstart, int step, int excl){
    block<double> cov;
-   for(int i = 0; i < len; i++){
+   for(int i = 0; i < len; i++){ // <-- needs an arbitrary start point
       double m = mu[i];
       for(int j = 0; j < sublen; j++){  // beter to avoid checks in tight inner loops, particularly if we use simd later
          double c = ts[i] - m;
          for(int k = 0; k < unroll; k++){
-            cov(k) += qbuff[j*unroll+k]*c;
+            cov(k) += qbuf[j*unroll+k]*c;
          } 
       }
-      // figure out which queries overlap the exclusion zone
-      int pref_end = max(0,(std::max(0,i-excl) - qstart)/step);
-      int suff_begin = max(0,(std::min(len-sublen+1,i+excl) - qstart)/step);
-      for(int k = 0; k < pref_end; k++){
-         int qi = qstart+k*step; // <-- I'll have to pass an offset pointer somewhere?
-         qcorr[qi] = cov(k);
-         qcov[qi] = cov(k-unroll);
-         qind[qi] = i;
-
+      m = invn[i];
+      for(int j = 0; j < unroll; j++){
+         cov(j+unroll) = cov(j)*m;
       }
-      for(int k = suff_begin; k < unroll; k++){
-         int qi = qstart+k*step; 
-         qcorr[qi] = cov(k);
-         qcov[qi] = cov(k-unroll);
-         qind[qi] = i;
-
+      for(int j = 0; j < unroll; j++){
+         cov(j+unroll) *= invn[qstart+j*step];
+      }
+      for(int k = 0; k < unroll; k++){
+         int qi = start+k*step;
+         if(cov(k+unroll) > corr[qi]){
+            corr[qi] = cov(k+unroll);
+            qmcov[qi]= cov(k);
+         }
+         if(qopt < cov(k+unroll)){ // <-- hello stall cycles
+            qopt = cov(k+unroll);
+            qopt_i = qi;
+         }
+      }
+      if(qopt > corr[i]){
+         corr[i] = qopt;
+         ind[i] = qopt_i;
       }
    }
 }
-
-
 
 
 // This takes a query range and preallocated buffers
@@ -88,14 +93,21 @@ static inline void partial_xcorr_kern(double* __Restrict__ qmcov, double* __Rest
 // In general we only care about the maxima here, which helps cut down on excess bus traffic
 void prescr_partial_xcorr(double* __Restrict__ qmcov, double* __Restrict__ qcorr, double* __Restrict__ qbuf, int64_t* __Restrict__ qind, const double* __Restrict__ ts, const double* __Restrict__ mu, const double* __Restrict__ invn, int qstart, int qcount, int len, int sublen, int step, int excl){
    int rem = qcount;
+   int maxqs = 8; // <-- Should be tuned using subsequence length and either detected cache size or dummy estimate
+
    while(rem > 0){
-      int unroll = std::min(8,rem);
+      int unroll = std::min(8,rem); // <-- this needs 2 steps of unrolling if we can reasonably stack enough queries to justify parallelizing this part
       int start = qstart+(qcount-rem)*step;
-      batch_normalize(ts,qbuf,mu,start,unroll,sublen,step);
-      partial_xcorr_kern(qmcov,qcorr,qbuf,qind,ts,mu,invn,start,step,excl); 
+      #pragma omp parallel for
+      for(int i = 0; i < unroll; i++){
+         batch_normalize(ts,qbuf,mu,start+i*unroll,unroll,sublen,step);  
+      }
+      #pragma omp parallel for
+      for(int i = 0; i < unroll; i++){  // <-- dummy setup
+         partial_xcorr_kern(qmcov,qcorr,qbuf,qind,ts,mu,invn,start,step,excl); 
+      }
       rem -= unroll;
    }
 }
-
 
 
