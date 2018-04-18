43417e9827972f29018273bb8dcb27016c9d3672
kavj <kjkamgar@gmail.com>
2018-04-18 12:06:40 -0700

    1/3 done

--- src/prescr_devel/prescr.cpp
+++ src/prescr_devel/prescr.cpp
@@ -2,42 +2,53 @@
 #include<cstdint>
 #include "../utils/reg.h"
 
-// better idea would be to make this neater then allow for MKL backend
 
-// I will clean up the __restrict__ and other nonsense later. They're mainly used by the compiler during auto-vectorization. 
-// There are a lot of different ways to set up this section in order to accommodate extremely long or short subsequence lengths and other things. 
 
-// Queries are interleaved by some unrolling factor. This way we can stride continuously through memory when computing initial covariance with unrolling
-// We would naturally use the upper triangular optimization so that it's easier to set exclusion zones. This means that for row 1, we would start with 1+excl given the symmetry of the whole thing
-// otherwise it gets really nasty
+// This could probably use some cleanup
 
-// doing this in a more naive manner confused gcc in that it was seemingly worried whether things might overlap
-void batch_normalize(double* __restrict__ qbuf,  const double* __restrict__ ts, const double* __restrict__ mu, int qstart, int count, int sublen, int step){
+void batch_normalize(double* __restrict__ qbuf,  const double* __restrict__ ts, const double* __restrict__ mu, const double* __restrict__ invn, int qstart, int count, int sublen, int step){
    #pragma omp simd  // <-- check whether syntax is valid stacked, possibly inline this without restrict quantifieers
    #pragma omp parallel for
    for(int i = 0; i < count; i++){
       int qind = i*sublen;
       int tsind = i*step+qstart;
       double qm = mu[qind];
+      double qinvn = invn[qind];
       for(int j = 0; j < sublen; j++){
-         qbuf[qind] = ts[tsind]-qm;
+         qbuf[qind] = (ts[tsind]-qm)*qinvn;
          ++qind;
          ++tsind;
       }
    }
 }
 
+
+// q is typically pre-normalized, but here we need centered only
+double autocov_single(const double* __restrict__ ts, double tmu, double qmu, int offset, int qoffset, int sublen){
+   double q = 0;
+   for(int i = 0; i < sublen; i++){
+      q += (ts[offset+i]-tmu)*(ts[qoffset+i]-qmu);
+   }
+   return q;
+}
+
+
 // prescrimp uses cross correlation, so it's only possible to fuse normalization and comparison
-void max_normalize_reduction(double* __restrict__ cov, double* __restrict__ invn, int* __restrict__ blah, double qcorr, double qinvn, int qind, int offset, int count){
-   #define unroll 8
+// we have a partially normalized cross correlation. We hoist chunks of 8 loads  then use a tiny decision tree for the intermediate reduction
+// I like it because it's geeky
+void max_conv_reduction(double* __restrict__ cov,  double* __restrict__ xcorr, int* __restrict__ cindex, double* __restrict__ invn, double qcorr, int qind, int qbase, int offset, int count){
+   const int unroll = 8;
    int aligned = count - count%unroll + offset;
    for(int i = offset; i < aligned; i+= unroll){
       block<double> corr;
       for(int j = 0; j < unroll; j++){
-         corr(j) = cov[i+j]*qinvn;
+         corr(j) = cov[i+j]*invn[i+j];
       }
       for(int j = 0; j < unroll; j++){
-         corr(j) *= invn[i+j];
+         if(xcorr[i+j] < corr(j)){
+            xcorr[i+j] = corr(j);
+            cindex[i+j] = qbase;
+         }
       }
       block<int> cind;
       for(int j = 0; j < 4; j++){
@@ -66,32 +77,65 @@ void max_normalize_reduction(double* __restrict__ cov, double* __restrict__ invn
          qind = cind(0);
       }
    }
-   *cov = qcorr;
-   *blah = qind; 
    for(int i = aligned; i < offset+count; i++){
-      double corr = cov[i]*qinvn*invn[i];
+      double corr = cov[i]*invn[i];
       if(qcorr < corr){
          qcorr = corr;
          qind =  i;
       } 
-   }
-   *blah = qind;  // <-- temporary so these aren't optimized out
-   *cov = qcorr;
-}
-
-
-/*
-void pxc_extrap_unifstride(double* __restrict__ qmcov, double* __restrict__ qcorr, double* __restrict__ qbuf, int64_t* __restrict__ qind, const double* __restrict__ ts, const double* __restrict__ mu, const double* __restrict__ df, const double* __restrict__ dg, const double* __restrict__ invn, int qstart, int qcount, int step, int mxlen){
-   for(int i = qstart; i < qcount; i++){
-      //int jmx = std::min(mxlen,i+step);
-      int jinit = qind[i];
-      //double c = 
-      for(int j = jinit; j < jinit+step; j++){
-         
+      if(xcorr[i] < corr){
+         xcorr[i] = corr;
+         cindex[i] = qbase;
       }
-   } 
+   }
+   cindex[qbase] = qind;  // <-- temporary so these aren't optimized out
+   xcorr[qbase] = qcorr;
 }
-*/
 
+
+// generally easiest to just figure the necessary starting index, then iterate forward only
+// put in limits
+void maxpearson_extrap(const double* __restrict__ qcov, const double* __restrict__ df, const double* __restrict__ dx, const double* __restrict__ invn, const int* __restrict__ qind, double* __restrict__ mp, int* __restrict__ mpi, int count, int stride, int extraplen){
+   for(int i = 0; i < count; i++){
+      int cind = qind[i]; 
+      int qind = i*stride;
+      double c = qcov[i];
+      for(int j = 0; j < extraplen; j++){
+         c += dx[cind]*df[qind];
+         c += dx[qind]*df[cind];
+         double scale = (invn[qind]*invn[cind]);
+         double corr = c*scale;
+         if(mp[cind] < corr){
+            mp[qind] = corr;
+            mpi[qind] = cind;
+         }
+         if(mp[qind] < corr){
+            mp[cind] = corr;
+            mpi[cind] = qind;
+         }
+         ++cind; 
+         ++qindd;
+      }
+      int cind = qind[i]; 
+      int qind = i*stride;
+      double c = qcov[i];
+      for(int j = 0; j < extraplen; j++){
+         c += dx[cind]*df[qind];
+         c += dx[qind]*df[cind];
+         double scale = (invn[qind]*invn[cind]);
+         double corr = c*scale;
+         if(mp[cind] < corr){
+            mp[qind] = corr;
+            mpi[qind] = cind;
+         }
+         if(mp[qind] < corr){
+            mp[cind] = corr;
+            mpi[cind] = qind;
+         }
+         --cind; 
+         --qindd;
+      }
+   }
+}
 
 
