560bac8db5215ad7369639ea0193383c93978e10
kavj <kjkamgar@gmail.com>
2018-04-23 08:26:01 -0700

    updated descriptors

--- src/prescr_devel/descriptors.h
+++ src/prescr_devel/descriptors.h
@@ -5,13 +5,14 @@
 
 
 
-
 struct qbuf{
    // Todo: explicit constructor and destructor. These can be assigned per block correlation section, so each section tracks a set of best so far reductions for the query set.
    // We require as many descriptors as max threads
    double* q;  
-   int qbufcount;
-   int qbufstride;
+   int bufct;
+   int qlen;
+   int bufstride;
+   int ct;
    double* qcov;  // covariance of optimal query match, multiple copies are used in the case of multiple queries
    double* qcorr; // need to know this
    int* qbase;    // stores the base index of each query with respect to the time series from which it was sampled
@@ -19,32 +20,29 @@ struct qbuf{
    int numcopies;    // since a lot of this is effectively read only (could enforce that) it is just strided, We don't allocate or deallocate in a shared section, and we avoid any cache line overlaps
 };
 
-struct xcorr_desc{
+struct p_autocorr_desc{
    double* xcorr;
    int* xind;
    VSLCorrTaskPtr* covdescs;
-   int seccount;
+   int sectct;
    int len;       //length of reduced cross correlation vector
-   int seclen;    //length of each correlation block section
+   int blocklen;    //length of each correlation block section
    int bufstride; //stride with respect to correlation buffer, a stride in memory accounts for fringe portions and elements used to pad memory alignment
-   int taillen;
-   int subseclen;
+   int taillen;   // <-- sections can be long so this is unpadded
+   int sublen;
    // should clean up task pointers as necessary ?
-};
 
-
-int init_taskptrs(struct xcorr_desc xcd, const double* ts){
-   int count = xcd.seccount;;
-   VSLCorrTaskPtr* covdescs = (VSLCorrTaskPtr*) mkl_malloc(count*sizeof(VSLCorrTaskPtr),64);
-   for(int i = 0; i < count; i++){
-      int status = vsldCorrNewTaskX1D(covdescs+i,VSL_CORR_MODE_FFT,xcd.seclen,xcd.subseclen,xcd.seclen,ts+i*xcd.bufstride,1);
-      if(status != VSL_STATUS_OK){
-         perror("could not initialize tasks");
-         return -1; 
+   int init_taskptrs(const double* ts, int len, int sublen){
+      covdescs = (VSLCorrTaskPtr*) mkl_malloc(sectct*sizeof(VSLCorrTaskPtr),64);
+      for(int i = 0; i < sectct; i++){
+         int status = vsldCorrNewTaskX1D(covdescs+i,VSL_CORR_MODE_FFT,blocklen,sublen,blocklen,ts+i*bufstride,1);
+         if(status != VSL_STATUS_OK){
+            perror("could not initialize tasks");
+            return -1; 
+         }
       }
-   }
-   xcd.covdescs = covdescs;
-   return 0;
-}
+      return 0;
+   }  
+};
 
 
