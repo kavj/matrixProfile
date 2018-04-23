19964bb0e6d75231a254faab6527b992c8d1745b
kavj <kjkamgar@gmail.com>
2018-04-23 07:59:01 -0700

    cleanup phase (fixing mismatches after updating data structures)

--- src/prescr_devel/descriptors.h
+++ src/prescr_devel/descriptors.h
@@ -3,88 +3,48 @@
 #include "mkl.h"
 
 
-// Todo: figure out a reasonable way to turn factoor out separate back ends for mkl vs quasi Cook-Toom filtration
 
 
-struct qdesc{
-   int qind;
-   double qcorr;
-   double qcov;
-};
 
 struct qbuf{
-   // this should probably have an explicit constructor and destructor
-
-   qbuf() : q(nullptr), qbufcount(0), qbufstride(0){}
-
+   // Todo: explicit constructor and destructor. These can be assigned per block correlation section, so each section tracks a set of best so far reductions for the query set.
+   // We require as many descriptors as max threads
    double* q;  
    int qbufcount;
    int qbufstride;
-  
-   // should have an explicit destructor
-
-   /*const double* getquery(int i){
-      if(i <= qbufcount){
-         return q+i*qbufstride;
-      }
-      else{
-         return nullptr;
-      }
-   }*/
-
-};
-
-struct buffer{
-   
-
-};
-
-
-struct qstatbuf{
-   // Todo: explicit constructor and destructor. These can be assigned per block correlation section, so each section tracks a set of best so far reductions for the query set.
-   // We require as many descriptors as max threads
-   
    double* qcov;  // covariance of optimal query match, multiple copies are used in the case of multiple queries
    double* qcorr; // need to know this
    int* qbase;    // stores the base index of each query with respect to the time series from which it was sampled
    int* qmatch;   // nearest neighbor index for each query
-
+   int numcopies;    // since a lot of this is effectively read only (could enforce that) it is just strided, We don't allocate or deallocate in a shared section, and we avoid any cache line overlaps
 };
 
 struct xcorr_desc{
    double* xcorr;
-   int* xcorrind;
-   int ccount;     //number of elements in correlation vector, in our cases (we truncate aliased portions) length(time_series) - length(query) + 1
-   int clen;       //length of reduced cross correlation vector
-   int cseclen;    //length of each correlation block section
-   int cbufstride; //stride with respect to correlation buffer, a stride in memory accounts for fringe portions and elements used to pad memory alignment
-   int sublen;
+   int* xind;
    VSLCorrTaskPtr* covdescs;
-
-
+   int seccount;
+   int len;       //length of reduced cross correlation vector
+   int seclen;    //length of each correlation block section
+   int bufstride; //stride with respect to correlation buffer, a stride in memory accounts for fringe portions and elements used to pad memory alignment
+   int taillen;
+   int subseclen;
    // should clean up task pointers as necessary ?
-
 };
 
 
-
-
-int init_taskptrs(VSLCorrTaskPtr* covdescs, double* ts, int clen, int seclen, int sublen){
-   int count = clen/cseclen;
-   if(count*cseclen < clen){
-      ++cseclen;
-   }
-   covdescs = (VSLCorrTaskPtr*) mkl_malloc(count*sizeof(VSLCorrTaskPtr),64);
-   for(int i = 0; i < ccount; i++){
-      int status = vsldCorrNewTaskX1D(covdescs+i,VSL_CORR_MODE_FFT,cseclen,sublen,cseclen,ts+i*cbufstride,1);
+int init_taskptrs(struct xcorr_desc xcd, const double* ts){
+   int count = xcd.seccount;;
+   VSLCorrTaskPtr* covdescs = (VSLCorrTaskPtr*) mkl_malloc(count*sizeof(VSLCorrTaskPtr),64);
+   for(int i = 0; i < count; i++){
+      int status = vsldCorrNewTaskX1D(covdescs+i,VSL_CORR_MODE_FFT,xcd.seclen,xcd.subseclen,xcd.seclen,ts+i*xcd.bufstride,1);
       if(status != VSL_STATUS_OK){
          perror("could not initialize tasks");
          return -1; 
       }
    }
+   xcd.covdescs = covdescs;
    return 0;
 }
-
-
 
 
