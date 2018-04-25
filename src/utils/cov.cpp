e2750ef20f5ac7bf90a64aa34ff331ba991788f9
kavj <kjkamgar@gmail.com>
2018-04-25 09:23:58 -0700

    updates

--- src/utils/cov.cpp
+++ src/utils/cov.cpp
@@ -1,10 +1,11 @@
 #include <cmath>
-#include "../arch/scalar.h"
+#include "../arch/avx256.h"
 #include "../utils/reg.h"
 
-using namespace scalar;//avx256_t;
+
+using namespace avx256_t;
 typedef double dtype;
-typedef double vtype;//__m256d vtype;
+typedef __m256d vtype;
 
 // todo: move reference to separate file, make scalar namespace
 
@@ -23,43 +24,33 @@ void batchcov_reference(const dtype* __restrict__ ts, dtype* cov, const dtype* _
    }
 }
 
-template<typename dtype, typename vtype,int unroll>
-static inline void batchcov_kern(const dtype* __restrict__ ts, dtype* __restrict__ cov, const dtype* __restrict__ query, const dtype* __restrict__ mu, int offset, int sublen){
-   const int simlen = sizeof(vtype)/sizeof(dtype);
-   block<vtype> c;
-   for(int i = offset; i < offset+sublen; i++){
-      vtype q = brdcst(mu,i-offset);
-      for(int j = 0; j < unroll; j++){
-         c(j+unroll) = uload(ts,i+simlen*j) - q;
-      }
-      q = brdcst(query,i);
-      for(int j = 0; j < unroll; j++){
-         c(j) = mul_add(q,c(j+unroll),c(j));
-      }
-   }
-   vtype q = brdcst(1.0);
-   for(int i = 0; i < unroll; i++){
-      astore(q/c(i),cov,offset+simlen*i);
-   }
-}
 
-//template<typename dtype,typename vtype>
-void batchcov(const dtype* __restrict__ ts, dtype* __restrict__ cov, const dtype* __restrict__ query, const dtype* __restrict__ mu, int offset, int count, int sublen){
-   const int unroll = 8;
+//Todo: cleanup edge handling
+template<typename dtype,typename vtype>
+void batchcov(const dtype* __restrict__ ts, dtype* __restrict__ cov, const dtype* __restrict__ query, const dtype* __restrict__ mu, int count, int sublen){
    const int simlen = sizeof(vtype)/sizeof(dtype);
    const int stride = simlen*unroll;
-   const int simd_block_count = count/stride;
-   const int last = count - simd_block_count*stride;   
-   if(simd_block_count > 0){
-      for(int i = offset; i < offset+count; i+=stride){  
-         batchcov_kern<dtype,vtype,unroll>(ts,cov,query,mu,i,sublen);
+   const int block_count = count/stride;
+   const int lastaligned = count - count%stride;
+   for(int i = 0; i < count; i+=stride){
+      block<vtype> c;
+      block<vtype> m;
+      for(int j = 0; j < unroll; j++){
+         m(j) = mu(i+j*simlen);
       }
-      if(offset+count > last){
-        //last-(count+offset)
-         batchcov_kern<dtype,vtype,1>(ts,cov,query,mu,last,sublen); 
+      for(int j = i; j < i+sublen; j++){
+         for(int k = 0; k < unroll; k++){
+            c(k+unroll) = uload(ts,j) - m(k);
+         }
+         for(int k = 0; k < unroll; k++){
+            c(k) = mul_add(uload(query,j),c(k+unroll),c(k));
+         }
+      }
+      for(int j = 0; j < unroll; j++){
+         ustore(c(k),cov,i+j*simlen);
       }
    }
-   else{
-      batchcov_reference(ts,cov,query,mu,offset,count,sublen);
-   }
+   // fringe should be done here. It would be possible to use a while loop countdown, like std::min(stride,count-i), and I need to add either a backshift or scalar loop that won't break
 }
+
+
