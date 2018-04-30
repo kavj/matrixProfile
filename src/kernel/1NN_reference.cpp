58f00a7fd39f0288df0223ac728dd19d3a95d2d1
kavj <kjkamgar@gmail.com>
2018-04-30 07:25:30 -0700

    just updating

--- src/kernel/1NN_reference.cpp
+++ src/kernel/1NN_reference.cpp
@@ -1,21 +1,162 @@
 #include<stdio.h>
 #include<cmath>
-
-typedef double dt;
-typedef long dti;
+#include "../utils/reg.h"
 // this could implement pairwise later
 
 // It could also be optimized to do sets of 8
 
 
-void maxpearson_reference(dt*  __restrict__ cx, const dt* __restrict__ mu, const dt* __restrict__ df, const dt* __restrict__ dx, const dt* __restrict__ invn, dt* __restrict__ mp, dti* __restrict__ mpi, int len, int diagmin, int sublen){
+
+/*
+void pears_reduct_avx(double* __restrict__ cov,  const double* __restrict__ df, const double* __restrict__ dx, const double* __restrict__ invn, double* __restrict__ xcorr, int* __restrict__ cindex, int cindoffset){
+   cov = (double*) __builtin_assume_aligned(cov,32);
+   df = (const double*) __builtin_assume_aligned(df,32);
+   dx = (const double*) __builtin_assume_aligned(dx,32);
+   invn = (const double*) __builtin_assume_aligned(invn,32);
+   xcorr = (double*) __builtin_assume_aligned(xcorr,32);
+   cindex = (int*) __builtin_assume_aligned(cindex,32); 
+   
+   const int unroll = 8;
+   const int count = 32;
+   const int aligned = count;
+   block<__m256d> cov_r;
+
+   for(int i = 0; i < aligned; i++){
+      for(int j = 0; j < unroll; j++){
+         cov_r(j) = aload(cov,j*4); 
+      } 
+      double q0 = brdcst(dx,i);
+      double q1 = brdcst(df,i);
+      for(int j = 0; j < unroll; j++){
+         cov_r(j) = mul_add(q0,uload(df,i+4*j+cindoffset));
+         cov_r(j) = mul_add(q1,uload(dx,i+4*j+cindoffset));
+      }
+      q0 = brdcst(uload(invn,i));
+      for(int j = 0; j < unroll; j++){
+         astore(cov_r(j),cov,i+4*j);
+         cov_r(j) *= q0;
+      }
+      for(int j = 0; j < unroll; j++){
+         corr(j) = cov_r(j)*uload(invn,i+j);
+      }
+      block<__m256d> cmpmsk;
+      for(int j = 0; j < unroll; j++){
+         cmpmsk(j) = x
+         if(uload(xcorr,i+4*j) < corr(j)){
+             = corr(j);
+            cindex[i+j] = i+cindoffset;
+         }
+      }
+      block<int> cind;
+      for(int j = 0; j < 4; j++){
+         if(corr(2*j) < corr(2*j+1)){
+            corr(2*j) = corr(2*j+1);
+            cind(j) = 2*j+1;
+         }
+         else{
+            cind(j) = 2*j;
+         }
+      }
+      for(int j = 0; j < 2; j++){
+         if(corr(4*j) < corr(4*j+2)){
+            corr(4*j) = corr(4*j+2);
+            cind(4*j) = cind(4*j+2);
+         }
+      }
+      if(corr(0) < corr(4)){
+         if(xcorr[i] < corr(4)){
+            xcorr[i] = corr(4);
+            cindex[i] = cind(4) + cindoffset;
+         }
+      }
+      else if(xcorr[i+cindoffset] < corr(0)){
+         xcorr[i] = corr(0);
+         cindex[i] = cind(0) + i;
+      }
+   }
+}
+*/
+
+
+
+void pears_reduct(double* __restrict__ cov,  const double* __restrict__ df, const double* __restrict__ dx, const double* __restrict__ invn, double* __restrict__ xcorr, int* __restrict__ cindex, int cindoffset){
+   cov = (double*) __builtin_assume_aligned(cov,32);
+   df = (const double*) __builtin_assume_aligned(df,32);
+   dx = (const double*) __builtin_assume_aligned(dx,32);
+   invn = (const double*) __builtin_assume_aligned(invn,32);
+   xcorr = (double*) __builtin_assume_aligned(xcorr,32);
+   cindex = (int*) __builtin_assume_aligned(cindex,32); 
+   
+   const int unroll = 8;
+   const int count = 32;
+   const int aligned = count;
+   block<double> cov_r;
+   for(int i = 0; i < unroll; i++){
+      cov_r(i) = cov[i];
+   }
+   for(int i = 0; i < aligned; i+= unroll){
+      double q0 = dx[i];
+      double q1 = df[i];
+      for(int j = 0; j < unroll; j++){
+         cov_r(j) += q0*df[i+j+cindoffset];
+         cov_r(j) += q1*dx[i+j+cindoffset];
+      }
+      block<double> corr;
+      q0 = invn[i];
+      for(int j = 0; j < unroll; j++){
+         corr(j) = cov_r(j)*q0;
+      }
+      for(int j = 0; j < unroll; j++){
+         corr(j) = cov_r(j)*invn[i+j];
+         cov[i+j] = corr(j);
+      }
+      for(int j = 0; j < unroll; j++){
+         if(xcorr[i+j] < corr(j)){
+            xcorr[i+j] = corr(j);
+            cindex[i+j] = i+cindoffset;
+         }
+      }
+      block<int> cind;
+      for(int j = 0; j < 4; j++){
+         if(corr(2*j) < corr(2*j+1)){
+            corr(2*j) = corr(2*j+1);
+            cind(j) = 2*j+1;
+         }
+         else{
+            cind(j) = 2*j;
+         }
+      }
+      for(int j = 0; j < 2; j++){
+         if(corr(4*j) < corr(4*j+2)){
+            corr(4*j) = corr(4*j+2);
+            cind(4*j) = cind(4*j+2);
+         }
+      }
+      if(corr(0) < corr(4)){
+         if(xcorr[i] < corr(4)){
+            xcorr[i] = corr(4);
+            cindex[i] = cind(4) + cindoffset;
+         }
+      }
+      else if(xcorr[i+cindoffset] < corr(0)){
+         xcorr[i] = corr(0);
+         cindex[i] = cind(0) + i;
+      }
+   }
+}
+
+
+
+   //  maxpearson_reference(double*,                  double const*,                 double const*,                 double const*,                   double*,                  int*,                  int,     int,         int)'
+
+void maxpearson_reference(double*  __restrict__ cov, const double* __restrict__ df, const double* __restrict__ dx, const double* __restrict__ invn, double* __restrict__ mp, int* __restrict__ mpi, int len, int diagmin, int sublen){
    for(int i = diagmin; i < len; i++){
-      dt cov = cx[i];
+      double c = cov[i];
       // if I don't update at initialization, I would need to do so here
       for(int j = i; j < len; j++){
          int k = j - i;
-         cov = cov + df[j]*dx[k] + df[k]*dx[j];
-         dt cor = cov*invn[j]*invn[k];
+         c = c + df[j]*dx[k] + df[k]*dx[j];
+         double cor = c*invn[j]*invn[k];
          if(mp[j] < cor){
             mp[j] = cor;
             mpi[j] = k;
--- src/prescr_devel/altapproach.cpp
+++ src/prescr_devel/altapproach.cpp
@@ -2,105 +2,61 @@
 #include "../utils/reg.h"
 using namespace avx256_t;
 
-/*
-void bleh2(double* __restrict__ cov, const double* __restrict__ df, const double* __restrict__ dx, const double* __restrict__ s, double* __restrict__ mp, int offsetr, int offsetc, int offsetmp){
-   cov = (double*)__builtin_assume_aligned(cov,32);
-   df = (const double*)__builtin_assume_aligned(df,32);
-   dx = (const double*)__builtin_assume_aligned(dx,32);
-   s = (const double*)__builtin_assume_aligned(s,32);
-   mp = (double*)__builtin_assume_aligned(mp,32);
-      
-   for(int j = 0; j < 64; j++){
-      cov[j] += dx[j]*df[j+offsetc];
-      cov[j] += df[j]*dx[j+offsetc];
-      if(mp[j] < cov[j]*s[j]*s[j+offsetc]){
-         mp[j] = cov[j]*s[j]*s[j+offsetc];
-      }
-      if(mp[j+offsetc] < cov[j]*s[j]*s[j+offsetc]){
-         mp[j+offsetc] = cov[j]*s[j]*s[j+offsetc];
-      }
-   }
-}
-*/
-
-/*
-void  bleh(double* __restrict__ cov, const double* __restrict__ df, const double* __restrict__ dx, const double* __restrict__ s, double* __restrict__ mp, int* __restrict__ mpi, int offsetr, int offsetc, int offsetmp){
-   cov = (double*)__builtin_assume_aligned(cov,32);
-   df = (const double*)__builtin_assume_aligned(df,32);
-   dx = (const double*)__builtin_assume_aligned(dx,32);
-   s = (const double*)__builtin_assume_aligned(s,32);
-   mp = (double*)__builtin_assume_aligned(mp,32);
-   mpi = (int*)__builtin_assume_aligned(mpi,32);
-   for(int j = 0; j < 64; j++){
-      cov[j] += dx[j]*df[j+offsetc];
-      cov[j] += df[j]*dx[j+offsetc];
-      if(mp[j] < cov[j]*s[j]*s[j+offsetc]){
-         mp[j] = cov[j]*s[j]*s[j+offsetc];
-         mpi[j] = offsetr+j;
-      }
-      if(mp[j+offsetc] < cov[j]*s[j]*s[j+offsetc]){
-         mp[j+offsetc] = cov[j]*s[j]*s[j+offsetc];
-         mpi[j+offsetc] = j;
-      }
-   }
-}*/
+struct rpair { __m256d val; __m256i index;};
 
 
-struct rpair {  __m256d val; __m256i index;};
-
-
-// AVX2 has 16 register names
-
+ 
 static inline struct rpair __attribute__((always_inline)) reduce(const __m256d& r0, const __m256d& r1, const __m256d& r2, const __m256d& r3,  const __m256d& r4, const __m256d& r5, const __m256d& r6, const __m256d& r7){
-   block<__m256i> mask;
-   mask(1) = r0 > r1;
-   mask(2) = r2 > r3;
-   mask(3) = r4 > r5;
-   mask(4) = r6 > r7;
-   block<__m256d> vals;
+
+   __m256i mask1, mask2, mask3, mask4, mask5, mask6;
+   __m256d  op1, op2, op3, op4;
+
+   mask1 = r0 > r1;
+   mask2 = r2 > r3;
+   mask3 = r4 > r5;
+   mask4 = r6 > r7;
+
    static const __m256i four = brdcst(4);
    static const __m256i eight = brdcst(8);
   
-   vals(1) = vmax(r0,r1);
-   vals(2) = vmax(r2,r3);
-   vals(3) = vmax(r4,r5);
-   vals(4) = vmax(r6,r7);
+   op1 = vmax(r0,r1);
+   op2 = vmax(r2,r3);
+   op3 = vmax(r4,r5);
+   op4 = vmax(r6,r7);
   
    __m256i g = set(0,1,2,3); 
    __m256i h = g + four;
-   mask(1) = blend(g,h,mask(1));
+   mask1 = blend(g,h,mask1);
    g += eight;
    h += eight;
-   mask(2) = blend(g,h,mask(2));
+   mask2 = blend(g,h,mask2);
    g += eight;
    h += eight;
-   mask(3) = blend(g,h,mask(3));
+   mask3 = blend(g,h,mask3);
    g += eight;
    h += eight;
-   mask(4) = blend(g,h,mask(4));
+   mask4 = blend(g,h,mask4);
    
-   mask(5) = vals(1) > vals(2);
-   mask(6) = vals(3) > vals(4);
+   mask5 = op1 > op2;
+   mask6 = op3 > op4;
    
-   vals(1) = vmax(vals(1),vals(2));
-   vals(3) = vmax(vals(3),vals(4));
+   op1 = vmax(op1,op2);
+   op3 = vmax(op3,op4);
 
-   mask(1) = blend(mask(1),mask(2),mask(5));
-   mask(3) = blend(mask(3),mask(4),mask(6));
+   mask1 = blend(mask1,mask2,mask5);
+   mask3 = blend(mask3,mask4,mask6);
    
    struct rpair x;
-   x.val = vmax(vals(1),vals(3));
-   x.index = blend(mask(1),mask(3),vals(1) > vals(3));
+   x.val = vmax(op1,op3);
+   x.index = blend(mask1,mask3,op1 > op3);
    return x;
 }
 
 
 
-
-
 #define tsz 64
 #define unroll 8
-void  bleh4(double* __restrict__ cov, const double* __restrict__ df, const double* __restrict__ dx, const double* __restrict__ s, double* __restrict__ mp, int* __restrict__ mpi, int offsetr, int offsetc, int offsetmp){
+void  symm_pearson_reduct_kern(double* __restrict__ cov, const double* __restrict__ df, const double* __restrict__ dx, const double* __restrict__ s, double* __restrict__ mp, int* __restrict__ mpi, int offsetr, int offsetc, int offsetmp){
    cov = (double*)__builtin_assume_aligned(cov,32);
    df = (const double*)__builtin_assume_aligned(df,32);
    dx = (const double*)__builtin_assume_aligned(dx,32);
@@ -137,24 +93,14 @@ void  bleh4(double* __restrict__ cov, const double* __restrict__ df, const doubl
          for(int k = 0; k < unroll; k+=2){
             __m256i r = brdcst(i);
             if(testnz(mask(k))){
-               if(testnz(mask(k+1))){
-                  maskstore(r,mask(k),mpi+i+j+4*k);
-                  maskstore(r,mask(k+1),mpi+i+j+4*k+4);
-                  maskstore(cov_r(k),mask(k),mp+i+j+4*k);
-                  maskstore(cov_r(k+1),mask(k+1),mp+i+j+4*k+4);
-               }
-               else{
-                  maskstore(cov_r(k),mask(k),mp+i+j+4*k);
-                  maskstore(r,mask(k),mpi+i+j+4*k+4);
-               }
-            }
-            else if(testnz(mask(k+1))){
                maskstore(cov_r(k),mask(k),mp+i+j+4*k);
-               maskstore(r,mask(k),mpi+i+j+4*k+4);
+               maskstore(r,mask(k),mpi+i+4*k);
             }
          }
          block<__m256d> c = cov_r;
          struct rpair r = reduce(c(0),c(1),c(2),c(3),c(4),c(5),c(6),c(7));
+         //Todo: technically this is incorrect, as we're writing back 4 operands rather than 1. It would be possible to do a final comparison against a temporary buffer and make one sweep at the end
+         // alternatively use the if statement to determine if we need to do a horizontal max (since that is quite expensive)
          __m256i msk = r.val > brdcst(mp,i+j);
          if(testnz(msk)){
             maskstore(r.val,msk,mp+i+j);
@@ -166,10 +112,8 @@ void  bleh4(double* __restrict__ cov, const double* __restrict__ df, const doubl
 
 
 
-
-
 #define tsz 64 
-void  bleh(double* __restrict__ cov, const double* __restrict__ df, const double* __restrict__ dx, const double* __restrict__ s, double* __restrict__ mp, int* __restrict__ mpi, int offsetr, int offsetc, int offsetmp){
+void  reference_pearson_reduc(double* __restrict__ cov, const double* __restrict__ df, const double* __restrict__ dx, const double* __restrict__ s, double* __restrict__ mp, int* __restrict__ mpi, int offsetr, int offsetc, int offsetmp){
    cov = (double*)__builtin_assume_aligned(cov,32);
    df = (const double*)__builtin_assume_aligned(df,32);
    dx = (const double*)__builtin_assume_aligned(dx,32);
@@ -199,205 +143,4 @@ void  bleh(double* __restrict__ cov, const double* __restrict__ df, const double
    }
 }
 
-
-/*
-void  bleh_avx(double* __restrict__ cov, const double* __restrict__ df, const double* __restrict__ dx, const double* __restrict__ s, double* __restrict__ mp, int* __restrict__ mpi, int offsetr, int offsetc, int offsetmp){
-   cov = (double*)__builtin_assume_aligned(cov,32);
-   df = (const double*)__builtin_assume_aligned(df,32);
-   dx = (const double*)__builtin_assume_aligned(dx,32);
-   s = (const double*)__builtin_assume_aligned(s,32);
-   mp = (double*)__builtin_assume_aligned(mp,32);
-   mpi = (int*)__builtin_assume_aligned(mpi,32);
-
-   int unroll = 32;
-   
-   for(int i = 0; i < tsz; i++){
-      block<__m256d> cov_r;
-      for(int j = 0; j < tsz; j+=unroll){
-         cov_r = aload(cov,4*j);
-      }
-      __m256d q = brdcst(dx,;
-      for(int j = 0; j < tsz; j+=unroll){
-         cov_r = 
-      }
-      block<__m256d> cov_r;
-      __m256d dx_r = brdcst(dx,i+j);
-      for(int j = 0; j < unroll; i++){
-         cov_r(i) = mul_add(dx_r,uload(df,i+j+
-      }  
-      for(int j = 0; j < tsz; j++){
-         cov[j] += dx[i+j]*df[i+j+offsetc];
-      }
-      for(int j = 0; j < tsz; j++){
-         cov[j] += df[i+j]*dx[i+j+offsetc];
-      }
-      for(int j = 0; j < tsz; j++){
-         if(mp[i+j] < cov[j]*s[i+j]*s[i+j+offsetc]){
-            mp[i+j] = cov[j]*s[i+j]*s[i+j+offsetc];
-            mpi[i+j] = offsetr+j;
-         }
-         if(mp[i+j+offsetc] < cov[j]*s[j]*s[j+offsetc]){
-            mp[i+j+offsetc] = cov[j]*s[j]*s[j+offsetc];
-            mpi[i+j+offsetc] = j;
-         }
-      }
-   }
-}
-*/
-
-
-
-
-
-
-/*
-void bleh(double* __restrict__ cov, const double* __restrict__ df, const double* __restrict__ dx, const double* __restrict__ s, double* __restrict__ mp, int* __restrict__ mpi, int offsetr, int offsetc, int offsetmp){
-   cov = (double*)__builtin_assume_aligned(cov,32);
-   df = (const double*)__builtin_assume_aligned(df,32);
-   dx = (const double*)__builtin_assume_aligned(dx,32);
-   s = (const double*)__builtin_assume_aligned(s,32);
-   mp = (double*)__builtin_assume_aligned(mp,32);
-   mpi = (int*)__builtin_assume_aligned(mpi,32);
-      
-   for(int j = 0; j < 64; j++){
-      cov[j] += dx[j]*df[j+offsetc];
-      cov[j] += df[j]*dx[j+offsetc];
-      if(mp[j] < cov[j]*s[j]*s[j+offsetc]){
-         mp[j] = cov[j]*s[j]*s[j+offsetc];
-         mpi[j] = offsetr+j;
-      }
-      if(mp[j+offsetc] < cov[j]*s[j]*s[j+offsetc]){
-         mp[j+offsetc] = cov[j]*s[j]*s[j+offsetc];
-         mpi[j+offsetc] = j;
-      }
-   }
-}
-*/
-
-
-/*void bleh(double* __restrict__ cov, const double* __restrict__ df, const double* __restrict__ dx, const double* __restrict__ s, double* __restrict__ mp, int* __restrict__ mpi, int offsetr, int offsetc, int offsetmp){
-   cov = (double*)__builtin_assume_aligned(cov,32);
-   df = (const double*)__builtin_assume_aligned(df,32);
-   dx = (const double*)__builtin_assume_aligned(dx,32);
-   s = (const double*)__builtin_assume_aligned(s,32);
-   mp = (double*)__builtin_assume_aligned(mp,32);
-   mpi = (int*)__builtin_assume_aligned(mpi,32);
-      
-   double corr[64];
-   for(int j = 0; j < 64; j++){
-      cov[j] += dx[j]*df[j+offsetc];
-   }
-   for(int j = 0; j < 64; j++){
-      cov[j] += df[j]*dx[j+offsetc];
-      corr[j] = cov[j]*s[j]*s[j+offsetc];
-   }
-   for(int j = 0; j < 64; j++){
-      if(corr[j] < mp[j]){
-         mp[j] = corr[j];
-         mpi[j] = offsetr+j;
-      }
-      if(corr[j] < mp[j+offsetc]){
-         mp[j+offsetc] = corr[j];
-         mpi[j+offsetc] = j+offsetc;
-      }
-   }
-}*/
-
-
-/*
-void bleh2(double* __restrict__ cov, const double* __restrict__ df, const double* __restrict__ dx, const double* __restrict__ s, double* __restrict__ mp, int* __restrict__ mpi, int offsetr, int offsetc, int offsetmp){
-   cov = (double*)__builtin_assume_aligned(cov,32);
-   df = (const double*)__builtin_assume_aligned(df,32);
-   dx = (const double*)__builtin_assume_aligned(dx,32);
-   s = (const double*)__builtin_assume_aligned(s,32);
-   mp = (double*)__builtin_assume_aligned(mp,32);
-   mpi = (int*)__builtin_assume_aligned(mpi,32);
-      
-   block<__m256d> r;
-   __m256d q = brdcst(dx,offsetc);
-   for(int j = 0; j < 8; j++){
-      r(j) = mul_add(q,aload(df,0),aload(cov,4*j));
-   }
-   q = brdcst(df,offsetc);
-   for(int j = 0; j < 8; j++){
-      r(j) = mul_add(q,aload(dx,0),r(j));
-   }
-   q = brdcst(s,offsetc);
-   for(int j = 0; j < 8; j++){
-      astore(r(j),cov,4*j);
-      r(j) *= q;
-   }
-   for(int j = 0; j < 8; j++){
-      r(j) *= uload(s,j);
-   }
-   for(int j = 0; j < 8; j++){
-      r(j) = vmax(r(j),aload(mp,4*j));
-      astore(r(j),mp,4*j);
-   }
-   
-}
-
-*/
-
-
-
-/*
-void bleh_long(const double* __restrict__ a, const double* __restrict__ b, const double* __restrict__ e, double* __restrict__ c, long* __restrict__ d, int len, int k){
-   a = (const double*)__builtin_assume_aligned(a,64);
-   b = (const double*)__builtin_assume_aligned(b,64);
-   c = (double*)__builtin_assume_aligned(c,64);
-   d = (long*)__builtin_assume_aligned(d,64);
-   e = (const double*)__builtin_assume_aligned(e,64);
-   for(int j = 0; j < 64; j++){
-   for(int i = 0; i < 64; i++){
-      c[i] += a[i+j]*b[j+k];
-   }
-   }
-
-*/
-  /* a = (double*)__builtin_assume_aligned(a,32);
-   b = (double*)__builtin_assume_aligned(b,32);
-   c = (double*)__builtin_assume_aligned(c,32);
-   d = (int*)__builtin_assume_aligned(d,32);
-
-
-   for(int i = 0; i < 32; i++){
-      for(int j = 0; j < 16; j++){
-  //       c[i] = a[i+j] + b[i+j]*e[i];    
-
-         if(a[i] < b[i]){
-            c[i] = b[i];
-            d[i] = j+k;
-         }
-      }
-  
-   }
-}
-*/
-/*
-void extrap(double* __restrict__ cov, double* __restrict__ mp, long* __restrict mpi, const double* __restrict__ s, const double* __restrict__ df, const double* __restrict__ dg, const double* __restrict__ dh, const double* __restrict dx,int offset){
-   cov = (double*) __builtin_assume_aligned(cov,32);
-   s = (const double*)__builtin_assume_aligned(s,32);
-   df = (const double*)__builtin_assume_aligned(df,32); 
-   dx = (const double*)__builtin_assume_aligned(dx,32);
-   dg = (const double*)__builtin_assume_aligned(dg,32);
-   dh = (const double*)__builtin_assume_aligned(dh,32);
-   mp = (double*)__builtin_assume_aligned(mp,32);
-   mpi= (long*)__builtin_assume_aligned(mpi,32);
-    
-   for(int l = 0; l < 32; l++){ 
-   for(int i = l; i < l+64; i++){
-      for(int j = 0; j < 8; j++){
-         cov[i+j] += df[i+j];
-      }
-      for(int j = 0; j < 16; j++){
-         cov[i+j] += dg[i+j]*dh[j];
-      }
-      
-   }
-   }
-}
-
-
-*/
 
