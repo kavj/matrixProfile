c0739e8cc8455b2b9a7b1746dd2f993cbba323a0
kavj <kjkamgar@gmail.com>
2018-05-21 10:41:48 -0700

    more updates, segfaults appear in main body

--- src/prescr_devel/exact_Pearson.h
+++ src/prescr_devel/exact_Pearson.h
@@ -18,18 +18,19 @@ int maxpearson_partialauto(stridedbuf<dtype>& ts, int minlag, int mlen, int subl
       printf("invalid time series\n");
    }
 
-   stridedbuf<dtype> mu(mlen), stridedbuf<dtype> invn(mlen), stridedbuf<dtype> df(mlen), stridedbuf<dtype> dx(mlen),
-   stridedbuf<dtype> mp(mlen), stridedbuf<dtype>  cov(mlen), stridedbuf<itype> mpi(mlen),stridedbuf<dtype> q(tilesperdim,sublen);
+   stridedbuf<dtype>mu(mlen); stridedbuf<dtype>invn(mlen); stridedbuf<dtype>df(mlen);  stridedbuf<dtype>dx(mlen);
+   stridedbuf<dtype>mp(mlen); stridedbuf<dtype>cov(mlen);  stridedbuf<itype>mpi(mlen); stridedbuf<dtype>q(tilesperdim,sublen);
 
    if(!(q.isvalid() && cov.isvalid() && mu.isvalid() && invn.isvalid() && df.isvalid()  && dx.isvalid() && mp.isvalid() && mpi.isvalid())){
       printf("could not assign objects\n");   
       return 0;
    } 
-   
+  
    ts.setstride(tsz), mu.setstride(tsz), df.setstride(tsz), dx.setstride(tsz), invn.setstride(tsz),
    mp.setstride(tsz), cov.setstride(tsz), mpi.setstride(tsz);
 
    xmean_windowed(ts(0),mu(0),ts.len,sublen);
+
    xsInv(ts(0),mu(0),invn(0),ts.len,sublen);   
    init_dfdx(ts(0), mu(0), df(0), dx(0),sublen,ts.len);
    std::fill(mp(0),mp(0)+mp.len,-1.0);
@@ -39,6 +40,8 @@ int maxpearson_partialauto(stridedbuf<dtype>& ts, int minlag, int mlen, int subl
    const int tstride = 65536;
    const int tcount = ts.len/tstride;  
    
+   // currently segfaults after this point
+ 
    #pragma omp parallel for
    for(int i = 0; i < tcount; i++){
       center_query(ts(i), mu(i), q(i), sublen);
