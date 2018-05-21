2dee186d8b7616def4b4bd3326a09ce9adc9083c
kavj <kjkamgar@gmail.com>
2018-05-21 09:40:52 -0700

    fixed memory allocation, updated constructor arguments, added a way to recalculate array stride after object creation

--- src/prescr_devel/descriptors.h
+++ src/prescr_devel/descriptors.h
@@ -14,23 +14,40 @@ static inline int   __attribute__((always_inline)) paddedlen(int buflen, int ali
 //I would prefer to avoid the use of late binding object types here due to the use of templating to overload precision of datatypes. This will probably be sufficient.
 
 template<typename dtype> struct stridedbuf{
-   //stridedbuf(int blen, int bcount, int stride){}
+   // This does allow for partial aliasing, which should obviously only be used in the case of pasing read only data structures to different threads
+   // If it's externally allocated, then we just want to let it determine the max number of blocks, we can do some minor error checking anyway
    dtype* dat;
    int bcount;
    int len;
    int stride;
-   // This does allow for partial aliasing, which should obviously only be used in the case of pasing read only data structures to different threads
-   // If it's externally allocated, then we just want to let it determine the max number of blocks, we can do some minor error checking anyway
+
    stridedbuf(dtype* a, int len, int stride, int count) : dat(a), len(len), stride(stride), bcount(count), ownsmem(false) {} 
 
-   stridedbuf(int count, int stride) : len(count*stride), bcount(count), stride(stride), ownsmem(true), dat(reinterpret_cast<dtype*>(init_buffer(paddedlen(sizeof(dtype)*count*stride,prefalign),prefalign))) {} 
+   stridedbuf(int count, int blocklen){
+      stride = paddedlen(blocklen,prefalign);
+      len = stride*count;
+      bcount = count;
+      dat = reinterpret_cast<dtype*>(init_buffer(stride*count*sizeof(dtype),prefalign));
+      ownsmem = true;
+   }
 
-   stridedbuf(int count, int stride, int len) : len(count*stride), bcount(count), stride(stride), ownsmem(true), dat(reinterpret_cast<dtype*>(init_buffer(paddedlen(sizeof(dtype)*count*stride,prefalign),prefalign))) {}
-
+   stridedbuf(int buflen){
+      stride = buflen;
+      len = buflen;
+      bcount = 1;
+      dat = reinterpret_cast<dtype*>(init_buffer(paddedlen(buflen,prefalign)*sizeof(dtype),prefalign));
+      ownsmem = true;
+   }
+   
    inline __attribute__((always_inline)) ~stridedbuf(){
       if(ownsmem){  //set this to something a bit more general 
          free(dat);
       }
+   }
+
+   inline __attribute__((always_inline)) void setstride(int st){
+      stride = st;
+      bcount = len/st + (len%stride ? 1 : 0);
    }
 
    inline __attribute__((always_inline)) bool isvalid() const {
@@ -45,12 +62,9 @@ template<typename dtype> struct stridedbuf{
       return i < bcount ? dat + i*stride : nullptr;
    }
 
-   inline dtype* __attribute__((always_inline)) operator()(int i, int stride){
-      return i*stride < len ? dat + i*stride : nullptr;
-   }
-
    private:
    bool ownsmem;
+   
 };
 
 
