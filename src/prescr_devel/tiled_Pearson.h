81dd0116ac588ebded5b1723bc767f1c6dccefa0
kavj <kjkamgar@gmail.com>
2018-05-26 00:29:08 -0700

    tidying up so Zach can test

--- src/prescr_devel/tiled_Pearson.h
+++ src/prescr_devel/tiled_Pearson.h
@@ -22,3 +22,17 @@ void pauto_pearson_reftest(
    int minlag,
    int mlen);
 
+
+
+void pauto_pearson(
+   double*       __restrict__ cov,
+   double*       __restrict__ mp,
+   long long*    __restrict__ mpi,
+   const double* __restrict__ df,
+   const double* __restrict__ dg,
+   const double* __restrict__ invn,
+   const int tlen,
+   const int offsetr,
+   const int offsetc,
+   const int upperbound);
+ 
