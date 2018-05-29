ca5adbbd4cf1a2fc19be822317a6d799a27773eb
kavj <kjkamgar@gmail.com>
2018-05-29 01:15:53 -0700

    skeleton for updated pearson function

--- src/prescr_devel/tiled_Pearson.cpp
+++ src/prescr_devel/tiled_Pearson.cpp
@@ -11,12 +11,12 @@
 
 // rename and reorganize later. This should be in a different namespace or compilation unit
 static inline void  pauto_pearson_AVX_kern (double* __restrict__ cov, double* __restrict__ mp, long long* __restrict__ mpi, const double* __restrict__ df, const double* __restrict__ dx, const double* __restrict__ invn, int offsetr, int offsetc){
-   cov = (double*)__builtin_assume_aligned(cov,32);
-   df  = (const double*)__builtin_assume_aligned(df,32);
-   dx  = (const double*)__builtin_assume_aligned(dx,32);
-   invn   = (const double*)__builtin_assume_aligned(invn,32);
-   mp  = (double*)__builtin_assume_aligned(mp,32);
-   mpi = (long long*)__builtin_assume_aligned(mpi,32);
+   cov = (double*)__builtin_assume_aligned(cov,prefalign);
+   df  = (const double*)__builtin_assume_aligned(df,prefalign);
+   dx  = (const double*)__builtin_assume_aligned(dx,prefalign);
+   invn   = (const double*)__builtin_assume_aligned(invn,prefalign);
+   mp  = (double*)__builtin_assume_aligned(mp,prefalign);
+   mpi = (long long*)__builtin_assume_aligned(mpi,prefalign);
    for(int i = 0; i < klen; i++){
       for(int j = 0; j < klen; j+=step){
          block<__m256d> cov_r;
@@ -64,6 +64,65 @@ static inline void  pauto_pearson_AVX_kern (double* __restrict__ cov, double* __
 
 
 
+static inline  __attribute__((always_inline)) void pauto_pearson_refkern_initial (
+   double*       __restrict__ cov,
+   double*       __restrict__ mp,
+   int*          __restrict__ mpi,
+   const double* __restrict__ df,
+   const double* __restrict__ dg,
+   const double* __restrict__ invn,
+   const int offsetr,
+   const int offsetc)
+{
+    cov =  (double*)__builtin_assume_aligned(cov,prefalign);
+    mp =   (double*)__builtin_assume_aligned(mp,prefalign);
+    mpi =  (int*)__builtin_assume_aligned(mpi,prefalign);
+    df =   (const double*)__builtin_assume_aligned(df,prefalign);
+    dg =   (const double*)__builtin_assume_aligned(dg,prefalign);
+    invn = (const double*)__builtin_assume_aligned(invn,prefalign);
+
+    // Note this goes by row then diagonal/column, so the inner block 
+    // determines the column. This messes with our normal variable naming convention 
+    // it's necessary so that values of cov may be updated independently
+
+   double corr[klen];
+   for(int j = 0; j < klen; j++){
+      corr[j] = cov[j]*invn[j+offsetc];
+   }
+   for(int j = 0; j < klen; j++){
+      corr[j] *= invn[0];
+   }
+   for(int i = 0; i < klen; i++){
+      for(int j = 1; j < klen; j++){
+         cov[j] += dg[i]*df[i+j+offsetc];
+      }
+      for(int j = 1; j < klen; j++){
+         cov[j] += df[i]*dg[i+j+offsetc];
+      } 
+      for(int j = 1; j < klen; j++){
+         corr[j] = cov[j]*invn[i+j+offsetc];
+      }
+      for(int j = 1; j < klen; j++){
+         corr[j] *= invn[i];
+      }
+      for(int j = 1; j < klen; j++){
+         if(mp[i] < corr[j]){
+            mp[i] = corr[j];
+            mpi[i] = i+j+offsetr+offsetc;
+         }
+      }
+      for(int j = 1; j < klen; j++){
+         if(mp[i+j+offsetc] < corr[j]){
+            mp[i+j+offsetc] = corr[j];
+            mpi[i+j+offsetc] = i+offsetr;
+         }
+      }
+   }
+}
+
+
+
+
 static inline  __attribute__((always_inline)) void pauto_pearson_refkern (
    double*       __restrict__ cov,
    double*       __restrict__ mp,
@@ -74,12 +133,12 @@ static inline  __attribute__((always_inline)) void pauto_pearson_refkern (
    const int offsetr,
    const int offsetc)
 {
-    cov =  (double*)__builtin_assume_aligned(cov,32);
-    mp =   (double*)__builtin_assume_aligned(mp,32);
-    mpi =  (int*)__builtin_assume_aligned(mpi,32);
-    df =   (const double*)__builtin_assume_aligned(df,32);
-    dg =   (const double*)__builtin_assume_aligned(dg,32);
-    invn = (const double*)__builtin_assume_aligned(invn,32);
+    cov =  (double*)__builtin_assume_aligned(cov,prefalign);
+    mp =   (double*)__builtin_assume_aligned(mp,prefalign);
+    mpi =  (int*)__builtin_assume_aligned(mpi,prefalign);
+    df =   (const double*)__builtin_assume_aligned(df,prefalign);
+    dg =   (const double*)__builtin_assume_aligned(dg,prefalign);
+    invn = (const double*)__builtin_assume_aligned(invn,prefalign);
 
     // Note this goes by row then diagonal/column, so the inner block 
     // determines the column. This messes with our normal variable naming convention 
@@ -128,13 +187,13 @@ static inline void pauto_pearson_edgekern(
    int offsetc, 
    int bound)
 {
-    cov =  (double*)__builtin_assume_aligned(cov,32);
-    mp =   (double*)__builtin_assume_aligned(mp,32);
-    mpi =  (int*)__builtin_assume_aligned(mpi,32);
-    df =   (const double*)__builtin_assume_aligned(df,32);
-    dg =   (const double*)__builtin_assume_aligned(dg,32);
-    invn = (const double*)__builtin_assume_aligned(invn,32);
-
+    cov =  (double*)__builtin_assume_aligned(cov,prefalign);
+    mp =   (double*)__builtin_assume_aligned(mp,prefalign);
+    mpi =  (int*)__builtin_assume_aligned(mpi,prefalign);
+    df =   (const double*)__builtin_assume_aligned(df,prefalign);
+    dg =   (const double*)__builtin_assume_aligned(dg,prefalign);
+    invn = (const double*)__builtin_assume_aligned(invn,prefalign);
+   
  
    int dlim = std::min(klen,bound);
    for(int i = 0; i < dlim; i++){
@@ -184,14 +243,12 @@ void pauto_pearson(
    const int tlen = 65536; // add in dynamic formula later
    const int kcount = tlen/klen;
    int tilesperdim = (mlen-minlag)/tlen;
-   int tailkcount = 0;
-   //int taillen = 0;
-   int fringe = 0;
+   int fringe = mlen - tilesperdim*tlen - minlag;
+   int tailkcount = kcount;
 
-   if(mlen - tilesperdim*tlen - minlag > 0){
-      int taillen = mlen - tilesperdim*tlen - minlag;      
-      fringe = taillen%klen;
-      tailkcount = (taillen - fringe)/klen;
+   if(fringe != 0){
+      tailkcount = fringe/klen;
+      fringe -= tailkcount*klen;
       ++tilesperdim;
    }
 
@@ -199,44 +256,41 @@ void pauto_pearson(
 
    #pragma omp parallel for
    for(int i = 0; i < tilesperdim; i++){
-      center_query(ts+i*tlen, mu[i*tlen], q(i), sublen);
+      center_query(ts+i*tlen, mu+i*tlen, q(i), sublen);
    }
 
    for(int d = 0; d < tilesperdim; d += tlen){
       #pragma omp parallel for
       for(int r = 0; r < tilesperdim - d; r += tlen){
-         if(d + r + 2 < tilesperdim){
-            // init normal
-            // so how are we going to deal with the first row annoyance?
-            for(int sd = 0; sd < kcount; sd++){
-               for(int sr = 0; sr < kcount; sr++){
-                  //pauto_pearson_kern();
-               }
-            }
+         bool inner = d + r + 2 < tilesperdim;
+         bool aligned_edge = d + r + 2 == tilesperdim;
+         int sdmx = (inner | aligned_edge) ? kcount : tailkcount;
+         if(inner || (edge && (tailkcount != 0))){
+            init_normal();
          }
-         else{   
-            // figure out if standard initialization is possible
-            int salign = (tilesperdim - d - r - 1)*kcount + tailcount;
-            if(salign > kcount){
-               // normal initialization then cap it to kcount
-               salign = kcount;
+         else{
+            init_edge();
+         }
+         for(int sd = 0; sd < sdmx; sd++){
+            int srmx = 0;
+            if(inner){
+               srmx = kcount;
+            }
+            else if(aligned_edge){
+               srmx = std::min(kcount + tailkcount - sd, kcount);
             }
             else{
                
             }
-            //salign = std::min(salign,kcount);
-            for(int sd = 0; sd < kcount; sd++){
-               int ralign = kcount + std::min(0,tailkcount-sd);
-               for(int sr = 0; sr < ralign; sr++){
-                  
-               }
-               if((fringe > 0) && (ralign < kcount)){  
-                  // edge_kern();
-               }
+            for(int sr = 1; sr < srmx; sr++){
+               pauto_pearson_inner();
+            } 
+            if((srmx != kcount) && (fringe != 0)){
+               pauto_pearson_edge();
             }
-            if((fringe > 0) && (salign < kcount)){
-               // edge_kern();
-            }
+         }
+         if((sdmx != kcount) && (fringe != 0)){
+            
          }
       }
    }
