ceec3b28bb5cd727fbdb085a338364cc5fd1585e
kavj <kjkamgar@gmail.com>
2018-06-03 10:37:42 -0700

    backing up, need to finish checking this over

--- src/prescr_devel/descriptors.h
+++ src/prescr_devel/descriptors.h
@@ -12,36 +12,49 @@ static inline int   __attribute__((always_inline)) paddedlen(int buflen, int ali
 
 //I would prefer to avoid the use of late binding object types here due to the use of templating to overload precision of datatypes. This will probably be sufficient.
 
+
+template<typename dtype> struct multibuf{
+
+   dtype* dat;
+   int bcount;
+   int stride;
+
+   multibuf(int count, int blocklen) : bcount(count) {
+      stride = paddedlen(blocklen,prefalign);
+      dat = reinterpret_cast<dtype*>(init_buffer(stride*count*sizeof(dtype),prefalign));
+   }
+   
+   inline __attribute__((always_inline)) ~multibuf(){
+      free(dat);
+   }
+
+   inline __attribute__((always_inline)) bool isvalid() const {
+      return (dat != nullptr);
+   }
+
+   inline dtype*  __attribute__((always_inline)) operator()(int i){ 
+      return i < bcount ? dat + i*stride : nullptr;
+   }
+
+};
+
+
 template<typename dtype> struct stridedbuf{
    // This does allow for partial aliasing, which should obviously only be used in the case of pasing read only data structures to different threads
    // If it's externally allocated, then we just want to let it determine the max number of blocks, we can do some minor error checking anyway
-   dtype* __restrict__ dat;
+   dtype* dat;
    int bcount;
-   int len;
    int stride;
+   int len;
 
-   stridedbuf(dtype* a, int len, int stride, int count) : dat(a), len(len), stride(stride), bcount(count), ownsmem(false) {} 
+   stridedbuf(dtype* a, int len, int stride, int count) : dat(a), len(len), stride(stride), bcount(count) {} 
 
-   stridedbuf(int count, int blocklen){
-      stride = paddedlen(blocklen,prefalign);
-      len = stride*count;
-      bcount = count;
-      dat = reinterpret_cast<dtype*>(init_buffer(stride*count*sizeof(dtype),prefalign));
-      ownsmem = true;
-   }
-
-   stridedbuf(int buflen){
-      stride = buflen;
-      len = buflen;
-      bcount = 1;
+   stridedbuf(int buflen) : stride(buflen), len(buflen), bcount(1) {
       dat = reinterpret_cast<dtype*>(init_buffer(paddedlen(buflen,prefalign)*sizeof(dtype),prefalign));
-      ownsmem = true;
    }
    
    inline __attribute__((always_inline)) ~stridedbuf(){
-      if(ownsmem){  //set this to something a bit more general 
-         free(dat);
-      }
+      free(dat);
    }
 
    inline __attribute__((always_inline)) void setstride(int st){
@@ -60,9 +73,6 @@ template<typename dtype> struct stridedbuf{
    inline dtype*  __attribute__((always_inline)) operator()(int i){ 
       return i < bcount ? dat + i*stride : nullptr;
    }
-
-   private:
-   bool ownsmem;
    
 };
 
--- src/prescr_devel/tiled_Pearson.h
+++ src/prescr_devel/tiled_Pearson.h
@@ -1,17 +1,30 @@
 
 
-void pauto_pearson(
+
+void pauto_pearson_inner(
    double*       __restrict__ cov,
    double*       __restrict__ mp,
    int*          __restrict__ mpi,
-   const double* __restrict__ ts,
-   const double* __restrict__ mu,
    const double* __restrict__ df,
    const double* __restrict__ dg,
    const double* __restrict__ invn,
    const int tlen,
    const int offsetr,
    const int offsetc);
+
+void pauto_pearson_edge(
+   double*       __restrict__ cov,
+   double*       __restrict__ mp,
+   int*          __restrict__ mpi,
+   const double* __restrict__ df,
+   const double* __restrict__ dg,
+   const double* __restrict__ invn,
+   const int tlen,
+   const int offsetr,
+   const int offsetc,
+   const int bound);
+
+
 
 void pauto_pearson_reftest(
    double*       __restrict__ cov,
@@ -24,16 +37,3 @@ void pauto_pearson_reftest(
    int mlen);
 
 
-
-void pauto_pearson(
-   double*       __restrict__ cov,
-   double*       __restrict__ mp,
-   long long*    __restrict__ mpi,
-   const double* __restrict__ ts,
-   const double* __restrict__ mu,
-   const double* __restrict__ df,
-   const double* __restrict__ dg,
-   const double* __restrict__ invn,
-   const int tlen,
-   const int offsetr,
-   const int offsetc); 
