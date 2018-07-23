aa1af73f3b38ba0d425fc523929b8d7cb2b94ad1
kavj <kjkamgar@gmail.com>
2018-07-23 07:51:40 -0700

    added pairwise reduction

--- src/solvers/pearson.cpp
+++ src/solvers/pearson.cpp
@@ -8,7 +8,7 @@
 #include "pearson.h"
 #define klen 64
 
-static void dfdg_init(const dtype* __restrict__ ts, const dtype* __restrict__ mu, dtype* __restrict__ df, dtype* __restrict__ dg, int len, int sublen){
+static void dfdg_init(const double* __restrict__ ts, const double* __restrict__ mu, double* __restrict__ df, double* __restrict__ dg, int len, int sublen){
    df[0] = 0;
    dg[0] = 0;
    for(int i = 0; i < len - sublen; i++){
@@ -35,11 +35,11 @@ void pauto_pearson_kern(
    invn = (const double*)__builtin_assume_aligned(invn, prefalign);
    
    for(int r = 0; r < klen; r++){
+      double a[klen];
       for(int d = 0; d < klen; d++){ 
          cov[d] += df[r] * dg[r + d + ofc];
          cov[d] += df[r + d + ofc] * dg[r];
       }
-      double a[klen];
       for(int d = 0; d < klen; d++){
          a[d] = cov[d] * invn[r] * invn[r + d + ofc];
       }
@@ -49,17 +49,34 @@ void pauto_pearson_kern(
             mpi[r + d + ofc] = r + ofr;
          }
       }
-      for(int d = 0; d < klen; d++){
-         if(mp[r] < a[d]){
-            mp[r] = a[d];
-            mpi[r] = r + d + ofr + ofc;
-	 }
+      long long e[klen];
+      for(int i = 0; i < 32; i++){
+         e[i] = a[i] < a[i + 32] ? i : i + 32;
+         a[i] = a[i] < a[i + 32] ? a[i] : a[i + 32];
+      } 
+      for(int i = 0; i < 16; i++){
+         e[i] = a[i] < a[i + 16] ? e[i] : e[i + 16];
+         a[i] = a[i] < a[i + 16] ? a[i] : a[i + 16];
       }
+      for(int i = 0; i < 8; i++){
+         e[i] = a[i] < a[i + 8] ? e[i] : e[i+8];
+         a[i] = a[i] < a[i + 8] ? a[i] : a[i+8];
+      }
+      for(int i = 0; i < 4; i++){
+         e[i] = a[i] < a[i + 4] ? e[i] : e[i + 4];
+         a[i] = a[i] < a[i + 4] ? a[i] : a[i + 4];
+      }
+      for(int i = 0; i < 2; i++){
+         e[i] = a[i] < a[i + 2] ? e[i] : e[i + 2];
+         a[i] = a[i] < a[i + 2] ? a[i] : a[i+2];
+      }
+      mpi[r] = mp[r] > a[0] ? mpi[r] : e[0] + ofr + ofc;
+      mp[r] =  mp[r] > a[0] ? mp[r] : a[0];
    }
 }
 
 
-static void pauto_pearson_edge(
+static inline void pauto_pearson_edge(
    double*       __restrict__ cov, 
    double*       __restrict__ mp,  
    int*          __restrict__ mpi, 
@@ -88,7 +105,7 @@ static void pauto_pearson_edge(
 }
 
 
-static void pauto_pearson_corner(
+static inline void pauto_pearson_corner(
    double*       __restrict__ cov, 
    double*       __restrict__ mp,  
    int*          __restrict__ mpi, 
@@ -114,7 +131,7 @@ static void pauto_pearson_corner(
 }
 
 
-static void pauto_pearson_init(
+static inline void pauto_pearson_init(
    double* __restrict__ cov, 
    const double* __restrict__ df,
    const double* __restrict__ dg,
@@ -186,7 +203,7 @@ int pearson_pauto_reduc(dsbuf& ts, dsbuf& mp, lsbuf& mpi, int minlag, int sublen
       return errs::bad_input;  // need to write the supporting namespace for this
    }
    const int mlen = ts.len - sublen + 1;
-   const int tlen = std::max(2 << 15, 4 * sublen - (4 * sublen) % klen);        
+   const int tlen = std::max(2 << 14, 4 * sublen - (4 * sublen) % klen);        
    const int tilesperdim = (mlen - minlag)/tlen + ((mlen - minlag) % tlen ? 1 : 0);
    dsbuf mu(mlen); dsbuf invn(mlen); dsbuf df(mlen);  
    dsbuf dg(mlen); dsbuf cov(mlen);  mdsbuf q(tilesperdim, sublen);
@@ -207,21 +224,21 @@ int pearson_pauto_reduc(dsbuf& ts, dsbuf& mp, lsbuf& mpi, int minlag, int sublen
       for(int ofst = 0; ofst < mlen - diag; ofst += tlen){
          const int dlim = std::min(tlen, mlen - diag - ofst);
          batchcov(ts(diag + ofst), mu(diag + ofst), q(ofst/tlen), cov(ofst), dlim, sublen);
-         const int dalim = std::min(tlen, kal - diag - ofst - klen);
+         const int dalim = std::max(0, std::min(tlen, kal - diag - ofst - klen));
          for(int d = diag; d < diag + dalim; d += klen){
             pauto_pearson_init(cov(d - diag + ofst), df(ofst), dg(ofst), mp(ofst), mpi(ofst), invn(ofst), ofst, d);
-            const int ralim = std::min(tlen, kal - d - ofst); 
-            printf("dalim: %d ralim %d\n",dalim, ralim);
+            const int ralim = std::max(0, std::min(tlen, kal - d - ofst)); 
+            //printf("dalim: %d ralim %d\n",dalim, ralim);
             for(int r = ofst + klen; r < ofst + ralim; r += klen){
                pauto_pearson_kern(cov(d - diag + ofst), mp(ofst), mpi(ofst), df(ofst), dg(ofst), invn(ofst), ofst, d);
             }
             if(ralim < tlen){
-               printf("edge: offset: %d, row lim: %d\n", ofst + ralim, mlen - d - ofst - ralim);
+         //      printf("edge: offset: %d, row lim: %d\n", ofst + ralim, mlen - d - ofst - ralim);
                pauto_pearson_edge(cov(d - diag + ofst + ralim), mp(ofst + ralim), mpi(ofst + ralim), df(ofst + ralim), dg(ofst + ralim), invn(ofst + ralim), ofst + ralim, d, klen, mlen - d - ofst - ralim);
             }   
          }
          if(dalim < tlen){
-            printf("corner: diag lim: %d column lim: %d\n", dlim - dalim, mlen - diag - dalim - ofst); 
+            //printf("corner: diag lim: %d column lim: %d\n", dlim - dalim, mlen - diag - dalim - ofst); 
             pauto_pearson_corner(cov(dalim + ofst), mp(ofst), mpi(ofst), df(ofst), dg(ofst), invn(ofst), ofst, diag + dalim, dlim - dalim, mlen - diag - dalim - ofst); 
          }
       }
