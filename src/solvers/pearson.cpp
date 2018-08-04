0b8e0ed1ab8af9fc4d3e4cd9e4fd975cd257b90f
kavj <kjkamgar@gmail.com>
2018-08-03 21:53:05 -0700

    reordered edge algorithm to match the reference, which blocks along row strips

--- src/solvers/pearson.cpp
+++ src/solvers/pearson.cpp
@@ -4,6 +4,7 @@
 #include "../utils/cov.h"
 #include "../utils/alloc.h"
 #include "../utils/primitive_print_funcs.h"
+#include "simd_tiled_pearson.h"
 #include "tiled_pearson.h"
 #include "pearson.h"
 constexpr long long klen  = 128; 
@@ -41,12 +42,6 @@ static void pauto_pearson_simple(
       }
       for(int d = 0; d < 128; d++){
          double cr = cov[d] * invn[r] * invn[r + d + ofc];
-         if((r + ofr == 78662) && (r + ofr + d + ofc >= 130700)){
-           // printf("r: %d r + ofr: %d d: %d d + ofc : %d r + d + ofr + ofc: %d\n", r, r + ofr, d, d + ofc, r + d + ofr + ofc);
-         } 
-         /*if((r + ofr >= 78592) && (r + d + ofr + ofc >= 130780)){
-            printf("check\n");
-         } */
          if(mp[r] < cr){
             mp[r] = cr;
             mpi[r] = r + d + ofr + ofc;
@@ -139,8 +134,8 @@ static inline void pauto_pearson_edge(
    const long long clim,
    const bool init)
 {
-   for(long long d = ofd; d < dlim; d++){
-      for(long long r = ofr; r < clim - d; r++){
+   for(long long r = ofr; r < clim - ofd; r++){
+      for(long long d = ofd; d < std::min(clim - r, dlim); d++){
          if(!init || r != ofr){
             cov[d - ofd] += df[r] * dg[r + d];
             cov[d - ofd] += df[r + d] * dg[r];
@@ -251,6 +246,7 @@ int pearson_pauto_reduc(dsbuf& ts, dsbuf& mp, lsbuf& mpi, long long minlag, long
             if(diag + klen <= dlim){
                const long long ral = std::max(static_cast<long long>(0), std::min(tlen, mlen - d - ofst - klen)); // stupid compiler
                pauto_pearson_kern(cov(ofst + d - diag), mp(ofst), mpi(ofst), df(ofst), dg(ofst), invn(ofst), ofst, d, ral);
+               //pauto_pearson_AVX_kern(cov(ofst), mp(ofst), mpi(ofst), df(ofst), dg(ofst), invn(ofst), ofst, d, ral);
                //pauto_pearson_simple(cov(ofst + d - diag), mp(ofst), mpi(ofst), df(ofst), dg(ofst), invn(ofst), ofst, d, ral); 
                if(ral < tlen){
                   pauto_pearson_edge(cov(ofst + d - diag), mp(0), mpi(0), df(0), dg(0), invn(0), ofst + ral, d, d + klen, mlen, false);
