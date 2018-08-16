7949922929576e356d4c861dad5bb54b4c5d5805
kavj <kjkamgar@gmail.com>
2018-08-16 04:42:04 -0700

    fixed indexing on intrinsics broadcasting, but it still skips some candidates for whatever reason

--- src/solvers/simd_tiled_pearson.h
+++ src/solvers/simd_tiled_pearson.h
@@ -31,58 +31,64 @@ void  pauto_pearson_AVX_kern (double*       __restrict__ cov,
       long long t2[simlen] = {0, 0, 0, 0};         
       struct rpair v;   
       for(int d = 0; d < 128; d += simlen * unroll){
-         block<__m256d> cov_r;
-         for(int sd = 0; sd < unroll; sd++){
-            cov_r(sd) = aload(cov, d + simlen * sd);
-         }
-         __m256d q;
+         fblock cov_r;
          if(r != 0){
-            q = brdcst(dg,r);
+            __m256d q = brdcst(dg, r);
+            fblock cv;
             for(int sd = 0; sd < unroll; sd++){
-               cov_r(sd) = mul_add(q, uload(df, r + d + ofc + simlen * sd), cov_r(sd));
+               cv(sd) = mul_add(q, uload(df, r + d + ofc + simlen * sd), aload(cov, d + simlen * sd));
             }
             q = brdcst(df, r);
             for(int sd = 0; sd < unroll; sd++){
-               cov_r(sd) = mul_add(q, uload(dg, r + d + ofc + simlen * sd), cov_r(sd));
-               prvec("cov_r", cov_r(sd));
+               cov_r(sd) = mul_add(q, uload(dg, r + d + ofc + simlen * sd), cv(sd));
+            }
+            for(int sd = 0; sd < unroll; sd++){
                astore(cov_r(sd), cov, d + simlen * sd);
             }
          }
-         q = brdcst(invn,r);
-         for(int sd = 0; sd < unroll; sd++){
-            cov_r(sd) *= q;
+         else{
+            for(int sd = 0; sd < unroll; sd++){
+               cov_r(sd) = aload(cov, d + simlen * sd);
+            }
          }
+         __m256d q = brdcst(invn, r);
+         fblock corr;
          for(int sd = 0; sd < unroll; sd++){
-            cov_r(sd) *= uload(invn, r + d + ofc + simlen * sd);
+            corr(sd) = cov_r(sd) * q * uload(invn, r + d + ofc + simlen * sd);
          }
-         block<__m256i> mask;
+         iblock mask;
          for(int sd = 0; sd < unroll; sd++){
-            mask(sd) = cov_r(sd) < uload(mp, r + d + ofc + simlen * sd);
+            mask(sd) = _mm256_castpd_si256(corr(sd) > uload(mp, r + d + ofc + simlen * sd));
          }
          __m256i s = brdcst(r + ofr);
          for(int sd = 0; sd < unroll; sd++){
-            if(testnz(mask(sd))){
-               maskstore(cov_r(sd), mask(sd), mp + r + d + simlen * sd + ofc);
+            if((r == 0) &&  (d + ofr + ofc == 29180)){
+               printf("check\n");
+            }
+            if(testz(mask(sd)) == 0){
+               maskstore(corr(sd), mask(sd), mp + r + d + simlen * sd + ofc);
                maskstore(s, mask(sd), mpi + r + d + ofc + simlen * sd);
             }
          }
-         struct rpair v = max_reduce_8x1(cov_r(0), cov_r(1), cov_r(2), cov_r(3), cov_r(4), cov_r(5), cov_r(6), cov_r(7));
-         __m256i msk = v.val > aload(&t1[0],0);
-         if(testnz(msk)){ 
+         struct rpair v = max_reduce_8x1(corr(0), corr(1), corr(2), corr(3), corr(4), corr(5), corr(6), corr(7));
+         __m256i msk = _mm256_castpd_si256(v.val > aload(&t1[0],0));
+         if(testz(msk) == 0){ 
             maskstore(v.val, msk, &t1[0]);
-            maskstore(v.index + brdcst(ofr + ofc), msk, &t2[0]);
+            maskstore(v.index + brdcst(r + d + ofr + ofc), msk, &t2[0]);
          }
       }
       __m256d t3 = aload(&t1[0],0) > brdcst(mp,r);
-      if(testnz(t3)){  
+      if(testz(t3) == 0){  
          double r0, r1;
-         double i0, i1;
-         i0 = t1[0] > t1[2] ? t1[0] : t1[2];
-         i1 = t1[1] > t1[3] ? t1[1] : t1[3];
-         r0 = t1[0] > t1[2] ? t1[0] : t1[2];
-         r1 = t1[1] > t1[3] ? t1[1] : t1[3];
-         mpi[r] = r0 > r1 ? i0 : i1;
-         mp[r] = r0 > r1 ? r0 : r1;
+         long long i0, i1;
+         i0 = t1[0] >= t1[1] ? t2[0] : t2[1];
+         i1 = t1[2] >= t1[3] ? t2[2] : t2[3];
+         r0 = t1[0] >= t1[1] ? t1[0] : t1[1];
+         r1 = t1[2] >= t1[3] ? t1[2] : t1[3];
+        /// if(r0 > mp[r] || r1 > mp[r]){
+         mpi[r] = r0 >= r1 ? i0 : i1;
+         mp[r] = r0 >= r1 ? r0 : r1;
+        // }
       } 
    }
 }
