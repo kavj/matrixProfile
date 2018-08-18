6fc4b3a59649054c8d6e794fbc2e30586da29f10
kavj <kjkamgar@gmail.com>
2018-08-18 00:45:06 -0700

    Added array initializer optimizations.

--- src/utils/cov.h
+++ src/utils/cov.h
@@ -1,23 +1,9 @@
 #include "../arch/avx256.h"
-#define prefalign 64
+#define prefalign 64 
 // Todo: Split reference and simd types
+#define wid 32
+#define unroll 8
 
+void center_query(const double* __restrict__ ts, const double* __restrict__ mu, double* __restrict__ q, int sublen);
 
-template<typename dtype, typename itype>
-void center_query(const dtype* __restrict__ ts, const dtype* __restrict__ mu, dtype* __restrict__ q, itype sublen){
-   q = (double*)__builtin_assume_aligned(q,prefalign);
-   for(itype i = 0; i < sublen; i++){
-      q[i] = ts[i] - mu[0];
-   }
-}
-
-template<typename dtype, typename itype>
-void batchcov(const dtype* __restrict__ ts, const dtype* __restrict__ mu, const dtype* __restrict__ query, dtype* __restrict__ cov, itype count, itype sublen){
-   for(itype i = 0; i < count; i++){
-      cov[i] = 0; 
-      for(itype j = 0; j < sublen; j++){
-         cov[i] += (ts[i + j] - mu[i]) * query[j];
-      }
-   }
-}
-
+void batchcov(const double* __restrict__ ts, const double* __restrict__ mu, const double* __restrict__ query, double* __restrict__ cov, int count, int sublen);
