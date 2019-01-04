b115420a5ef8b39ce409f5251e75733725e6b85c
kavj <kjkamgar@gmail.com>
2019-01-04 03:56:10 -0800

    cleanup

--- src/utils/alloc.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef  __ALLOCATORS__
-#define __ALLOCATORS__
-constexpr const int prefalign = 64;
-int paddedlen(int len);
-void* alloc_aligned_buffer(int buflen);
-void  dealloc_aligned_buffer(void* buf);
-#endif
--- src/utils/primitive_print_funcs.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PRIM_PRINT__
-#define __PRIM_PRINT__
-void writeDoubles(const char* name, const double* t, const int n);
-void appendDoubles(const char* name, const double* t, const int n);
-void writeInts(const char* name, const int* t, int n);
-void writeLongs(const char* name, const long long* t, const int n);
-#endif
-
-
