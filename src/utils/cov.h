bdbfc7a3e697366e4228ff5f6e2bcaaf38834f59
kavj <kjkamgar@gmail.com>
2019-01-14 11:26:44 -0800

    getting rid of wrong files

--- src/utils/cov.h
+++
@@ -1,8 +0,0 @@
-#define prefalign 64 
-// Todo: Split reference and simd types
-#define wid 32
-#define unroll 8
-
-void center_query(const double* restrict ts, const double* restrict mu, double* restrict q, int sublen);
-
-void batchcov(const double* restrict ts, const double* restrict mu, const double* restrict query, double* restrict cov, int count, int sublen);
